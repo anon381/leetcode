54d93a91402cca3347d184d624caa186a7d4fe2a
Yabets Maregn <yabetsanon@gmail.com>
2025-03-19 08:18:00 +0900

    added cpp code

--- 1749. Maximum Absolute Sum of Any Subarray.py
+++ 1749. Maximum Absolute Sum of Any Subarray.py
@@ -2,4 +2,16 @@ class Solution:
     def maxAbsoluteSum(self, A):
         return max(accumulate(A, initial=0)) - min(accumulate(A, initial=0))        
 
-# C++ version 
+# C++ version
+# class Solution {
+# public:
+#     int maxAbsoluteSum(vector<int>& A) {
+#         int max_sum = 0, min_sum = 0, curr_sum = 0;
+#         for (int x : A) {
+#             curr_sum += x;
+#             max_sum = max(max_sum, curr_sum);
+#             min_sum = min(min_sum, curr_sum);
+#         }
+#         return max_sum - min_sum;
+#     }
+# };
