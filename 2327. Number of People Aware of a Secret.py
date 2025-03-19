1a11fa42f0a3dfb9a9d00195b6bd14c0643ff9ef
Yabets Maregn <yabetsanon@gmail.com>
2025-03-19 12:00:00 +0900

    Added cpp code

--- 2327. Number of People Aware of a Secret.py
+++ 2327. Number of People Aware of a Secret.py
@@ -19,3 +19,32 @@ class Solution:
         return ans
 
 # C++ version 
+# #include <vector>
+# using namespace std;
+# class Solution {
+# public:
+#     int peopleAwareOfSecret(int n, int delay, int forget) {
+#         const int MOD = 1e9 + 7;
+#         if (n == 1) return 1;
+#         vector<int> dp(n + 1, 0);
+#         dp[1] = 1;
+#         int window = 0;
+#         for (int i = 2; i <= n; ++i) {
+#             int enter = i - delay;
+#             int exit_ = i - forget;
+#             if (enter >= 1) {
+#                 window = (window + dp[enter]) % MOD;
+#             }
+#             if (exit_ >= 1) {
+#                 window = (window - dp[exit_] + MOD) % MOD;
+#             }
+#             dp[i] = window;
+#         }
+#         int start = max(1, n - forget + 1);
+#         long long ans = 0;
+#         for (int i = start; i <= n; ++i) {
+#             ans = (ans + dp[i]) % MOD;
+#         }
+#         return ans;
+#     }
+# };
