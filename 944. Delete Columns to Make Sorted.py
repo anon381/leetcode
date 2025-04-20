2bd505081d17c4e45372bac762ff268c019c0366
Yabets Maregn <yabetsanon@gmail.com>
2025-04-20 12:00:00 +0900

    Add C++ version as comment: 944. Delete Columns to Make Sorted.py

--- 944. Delete Columns to Make Sorted.py
+++ 944. Delete Columns to Make Sorted.py
@@ -1,3 +1,25 @@
 class Solution:
     def minDeletionSize(self, A: List[str]) -> int:
         return len([True for col in zip(*A) if sorted(col) != list(col)])
+
+# C++ version of the above Python code:
+#
+# #include <vector>
+# #include <string>
+# using namespace std;
+# class Solution {
+# public:
+#     int minDeletionSize(vector<string>& A) {
+#         int count = 0;
+#         int n = A.size(), m = A[0].size();
+#         for (int col = 0; col < m; ++col) {
+#             for (int row = 1; row < n; ++row) {
+#                 if (A[row][col] < A[row-1][col]) {
+#                     count++;
+#                     break;
+#                 }
+#             }
+#         }
+#         return count;
+#     }
+# };
