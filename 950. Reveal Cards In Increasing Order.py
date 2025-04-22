b1e33db2a707d02e1a6ca7599dfe2976f230908c
Yabets Maregn <yabetsanon@gmail.com>
2025-04-22 12:00:00 +0900

    Add C++ version as comment: 950. Reveal Cards In Increasing Order.py

--- 950. Reveal Cards In Increasing Order.py
+++ 950. Reveal Cards In Increasing Order.py
@@ -13,3 +13,28 @@ class Solution:
                 indices.append(indices.popleft())
 
         return result
+
+# C++ version of the above Python code:
+#
+# #include <vector>
+# #include <deque>
+# #include <algorithm>
+# using namespace std;
+# class Solution {
+# public:
+#     vector<int> deckRevealedIncreasing(vector<int>& deck) {
+#         sort(deck.begin(), deck.end());
+#         int n = deck.size();
+#         deque<int> indices;
+#         for (int i = 0; i < n; ++i) indices.push_back(i);
+#         vector<int> result(n);
+#         for (int card : deck) {
+#             int idx = indices.front(); indices.pop_front();
+#             result[idx] = card;
+#             if (!indices.empty()) {
+#                 indices.push_back(indices.front()); indices.pop_front();
+#             }
+#         }
+#         return result;
+#     }
+# };
