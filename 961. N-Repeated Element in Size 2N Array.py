1a7e24f1959a0720e2dfe75426bdc998dd579ed3
Yabets Maregn <yabetsanon@gmail.com>
2025-04-25 12:00:00 +0900

    Add C++ version as comment: 961. N-Repeated Element in Size 2N Array.py

--- 961. N-Repeated Element in Size 2N Array.py
+++ 961. N-Repeated Element in Size 2N Array.py
@@ -3,4 +3,22 @@ class Solution:
         while 1:
             s = random.sample(A, 2)
             if s[0] == s[1]:
-                return s[0]        
+                return s[0]
+
+# C++ version of the above Python code:
+#
+# #include <vector>
+# #include <unordered_set>
+# using namespace std;
+# class Solution {
+# public:
+#     int repeatedNTimes(vector<int>& A) {
+#         unordered_set<int> seen;
+#         for (int num : A) {
+#             if (seen.count(num)) return num;
+#             seen.insert(num);
+#         }
+#         return -1; // Should not reach here
+#     }
+# };
+#
