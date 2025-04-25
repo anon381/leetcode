077bba18e94d098f215b8cacd0b2580a16e6a02b
Yabets Maregn <yabetsanon@gmail.com>
2025-04-25 12:00:00 +0900

    Add Java version as comment: 961. N-Repeated Element in Size 2N Array.py

--- 961. N-Repeated Element in Size 2N Array.py
+++ 961. N-Repeated Element in Size 2N Array.py
@@ -22,3 +22,17 @@ class Solution:
 #     }
 # };
 #
+# Java version of the above Python code:
+#
+# import java.util.*;
+# class Solution {
+#     public int repeatedNTimes(int[] A) {
+#         Set<Integer> seen = new HashSet<>();
+#         for (int num : A) {
+#             if (seen.contains(num)) return num;
+#             seen.add(num);
+#         }
+#         return -1; // Should not reach here
+#     }
+# }
+#
