b4a2af286390065e670a6e5e7b2fcd1ae670907c
Yabets Maregn <yabetsanon@gmail.com>
2025-05-11 12:00:00 +0900

    added java code

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -18,3 +18,17 @@ class Solution:
 
 
             #in java
+            # import java.util.*;
+            # class Solution {
+            #     public int[] twoSum(int[] nums, int target) {
+            #         Map<Integer, Integer> pairIdx = new HashMap<>();
+            #         for (int i = 0; i < nums.length; i++) {
+            #             if (pairIdx.containsKey(target - nums[i])) {
+            #                 return new int[]{i, pairIdx.get(target - nums[i])};
+            #             }
+            #             pairIdx.put(nums[i], i);
+            #         }
+            #         throw new IllegalArgumentException("No two sum solution");
+            #     }
+            # }
+        
