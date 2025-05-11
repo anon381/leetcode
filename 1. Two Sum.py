1cb8deb0a71f1e64635eaa5dcd569866b63121e4
Yabets Maregn <yabetsanon@gmail.com>
2025-05-11 12:00:00 +0900

    added cpp code

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -8,3 +8,10 @@ class Solution:
             pair_idx[num] = i
 
             #in cpp
+            # unordered_map<int, int> pair_idx;
+            # for (int i = 0; i < nums.size(); ++i) {
+            #     if (pair_idx.count(target - nums[i])) {
+            #         return {i, pair_idx[target - nums[i]]};
+            #     }
+            #     pair_idx[nums[i]] = i;
+            # }
