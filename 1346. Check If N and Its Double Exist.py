acd3c24e88eb446608940279f2351f226e1f3949
Yabets Maregn <yabetsanon@gmail.com>
2025-05-16 12:00:00 +0900

    Add C++ version as comment: 1346. Check If N and Its Double Exist

--- 1346. Check If N and Its Double Exist.py
+++ 1346. Check If N and Its Double Exist.py
@@ -7,4 +7,24 @@ class Solution:
                 return True  
             seen[num] = seen.get(num, 0) + 1
 
-        return False  
+        return False
+
+# C++ version of the above Python code:
+#
+# #include <vector>
+# #include <unordered_set>
+# using namespace std;
+# class Solution {
+# public:
+#     bool checkIfExist(vector<int>& arr) {
+#         unordered_set<int> seen;
+#         for (int num : arr) {
+#             if (seen.count(2 * num) || (num % 2 == 0 && seen.count(num / 2))) {
+#                 return true;
+#             }
+#             seen.insert(num);
+#         }
+#         return false;
+#     }
+# };
+#
