899c62f2760f8a9aef1ad3f5cf98da2e5b9330ee
Yabets Maregn <yabetsanon@gmail.com>
2025-05-18 12:00:00 +0900

    Add C++ version as comment: 1365. How Many Numbers Are Smaller Than the Current Number

--- 1365. How Many Numbers Are Smaller Than the Current Number.py
+++ 1365. How Many Numbers Are Smaller Than the Current Number.py
@@ -13,3 +13,29 @@ class Solution:
             ret.append(d[i])      
 
         return ret
+
+# C++ version of the above Python code:
+#
+# #include <vector>
+# #include <algorithm>
+# #include <unordered_map>
+# using namespace std;
+# class Solution {
+# public:
+#     vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+#         vector<int> temp = nums;
+#         sort(temp.begin(), temp.end());
+#         unordered_map<int, int> d;
+#         for (int i = 0; i < temp.size(); ++i) {
+#             if (d.find(temp[i]) == d.end()) {
+#                 d[temp[i]] = i;
+#             }
+#         }
+#         vector<int> ret;
+#         for (int num : nums) {
+#             ret.push_back(d[num]);
+#         }
+#         return ret;
+#     }
+# };
+#
