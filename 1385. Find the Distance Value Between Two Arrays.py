9e93e069022e1a5f12115a8fafbfc17a81c75ffa
Yabets Maregn <yabetsanon@gmail.com>
2025-05-19 12:00:00 +0900

    Add C++ version as comment: 1385. Find the Distance Value Between Two Arrays

--- 1385. Find the Distance Value Between Two Arrays.py
+++ 1385. Find the Distance Value Between Two Arrays.py
@@ -12,5 +12,27 @@ class Solution:
         
         return count
 
+# C++ version of the above Python code:
+#
+# #include <vector>
+# #include <algorithm>
+# #include <cmath>
+# using namespace std;
+#
+# class Solution {
+# public:
+#     int findTheDistanceValue(vector<int>& arr1, vector<int>& arr2, int d) {
+#         sort(arr2.begin(), arr2.end());
+#         int count = 0;
+#         for (int num : arr1) {
+#             auto left = lower_bound(arr2.begin(), arr2.end(), num - d);
+#             auto right = upper_bound(arr2.begin(), arr2.end(), num + d);
+#             if (left == right) {
+#                 count++;
+#             }
+#         }
+#         return count;
+#     }
+# };
 
-        
+
