f890f8718a5a0338abdebc2e7837cccb9fd6a3ae
Yabets Maregn <yabetsanon@gmail.com>
2025-05-25 12:00:00 +0900

    Add Java version as comment: 1365. How Many Numbers Are Smaller Than the Current Number

--- 1365. How Many Numbers Are Smaller Than the Current Number.py
+++ 1365. How Many Numbers Are Smaller Than the Current Number.py
@@ -39,3 +39,24 @@ class Solution:
 #     }
 # };
 #
+# Java version of the above Python code:
+#
+# import java.util.*;
+# class Solution {
+#     public int[] smallerNumbersThanCurrent(int[] nums) {
+#         int[] temp = nums.clone();
+#         Arrays.sort(temp);
+#         Map<Integer, Integer> d = new HashMap<>();
+#         for (int i = 0; i < temp.length; i++) {
+#             if (!d.containsKey(temp[i])) {
+#                 d.put(temp[i], i);
+#             }
+#         }
+#         int[] ret = new int[nums.length];
+#         for (int i = 0; i < nums.length; i++) {
+#             ret[i] = d.get(nums[i]);
+#         }
+#         return ret;
+#     }
+# }
+#
