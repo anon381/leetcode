991b3975accfc5257b6f975c691ae9a16dc5f103
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2025-08-14 17:55:14 +0900

    Update 87. Scramble String.py

--- 87. Scramble String.py
+++ 87. Scramble String.py
@@ -4,9 +4,11 @@ class Solution:
         n = len(s1)
         if s1 == s2:
             return True
+            
         a, b, c = [0] * 26, [0] * 26, [0] * 26
         if (s1 + s2) in self.map:
             return self.map[s1 + s2]
+            
         for i in range(1, n):
             j = n - i
             a[ord(s1[i - 1]) - ord('a')] += 1
@@ -15,8 +17,10 @@ class Solution:
             if a == b and self.isScramble(s1[:i], s2[:i]) and self.isScramble(s1[i:], s2[i:]):
                 self.map[s1 + s2] = True
                 return True
+                
             if a == c and self.isScramble(s1[:i], s2[j:]) and self.isScramble(s1[i:], s2[:j]):
                 self.map[s1 + s2] = True
                 return True
+                
         self.map[s1 + s2] = False
         return False
