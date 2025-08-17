95ea9ab5e7b432f81a013274ba19042e39261d4d
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2025-08-17 21:09:22 +0900

    Update 851. Loud and Rich.py

--- 851. Loud and Rich.py
+++ 851. Loud and Rich.py
@@ -1,1 +1,30 @@
 
+
+class Solution:
+    def loudAndRich(self, richer: List[List[int]], quiet: List[int]) -> List[int]:
+        total = len(quiet)
+        network = [[] for _ in range(total)]
+        incoming = [0] * total
+        output = list(range(total))
+        
+        for x, y in richer:
+            network[x].append(y)
+            incoming[y] += 1
+        
+        dq = deque()
+        for person in range(total):
+            if incoming[person] == 0:
+                dq.append(person)
+        
+        while dq:
+            current = dq.popleft()
+            
+            for nxt in network[current]:
+                if quiet[output[current]] < quiet[output[nxt]]:
+                    output[nxt] = output[current]
+                
+                incoming[nxt] -= 1
+                if incoming[nxt] == 0:
+                    dq.append(nxt)
+        
+        return output
