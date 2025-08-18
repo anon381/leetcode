bfafd05494f78102a2a77712a388825a710b8e33
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2025-08-18 20:38:41 +0900

    Update 589. N-ary Tree Preorder Traversal.py

--- 589. N-ary Tree Preorder Traversal.py
+++ 589. N-ary Tree Preorder Traversal.py
@@ -1,1 +1,21 @@
 
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        if not root:
+            return []
+        stack = [root]
+        l = []
+        while stack:
+            node = stack.pop()
+            l.append(node.val)
+            for child in node.children[::-1]:
+                stack.append(child)
+        return l
