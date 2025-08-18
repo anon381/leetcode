9e0d673d16dbb0eb433b2124e69aea99056d6c6f
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2025-08-18 20:33:33 +0900

    Update 563. Binary Tree Tilt.py

--- 563. Binary Tree Tilt.py
+++ 563. Binary Tree Tilt.py
@@ -1,19 +1,16 @@
-
 class Solution:
     def findTilt(self, root: Optional[TreeNode]) -> int:
-        self.total_tilt = 0
-        
+        self.total_tilt = 0  
         def calculate_tilt(node):
             if not node:
-                return 0
-            
+                return 0 
             left_sum = calculate_tilt(node.left)
             right_sum = calculate_tilt(node.right)
             tilt = abs(left_sum - right_sum)
-            
+            #calculate the sums and tilt
             self.total_tilt += tilt
-            
+            #return the sum
             return left_sum + right_sum + node.val
-        
+        #calculate tilt
         calculate_tilt(root)
         return self.total_tilt
