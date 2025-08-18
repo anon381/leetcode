b4366d3e749ae838d44a9886f9b22563bb04a12d
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2025-08-18 20:36:17 +0900

    Update 572. Subtree of Another Tree.py

--- 572. Subtree of Another Tree.py
+++ 572. Subtree of Another Tree.py
@@ -1,10 +1,6 @@
 
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        # Read the Main Method, before reading the helper Method.
-
-        # Helper Method:
-        # DFS, Checking whether two trees are same
         def dfs_sameTree(node1, node2):
             if node1 and node2:
                 if node1.val != node2.val:
@@ -16,15 +12,12 @@ class Solution:
             else:
                 return False
 
-
-        # Main Method:
-        # Finding the matching root for the subRoot
         stack, rootNode = [root], None
         while stack:
             rootNode = stack.pop()
             if not rootNode: continue
 
-            # Once we have a match,  We check whether they are same tree.
+
             if rootNode.val == subRoot.val and dfs_sameTree(rootNode, subRoot): 
                 return True
                 
