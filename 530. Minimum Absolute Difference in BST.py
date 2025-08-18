0b030228716f23dcf41705346b001e8f3e0db2a6
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2025-08-18 20:30:13 +0900

    Update 530. Minimum Absolute Difference in BST.py

--- 530. Minimum Absolute Difference in BST.py
+++ 530. Minimum Absolute Difference in BST.py
@@ -1,11 +1,9 @@
 class Solution:
     def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
-
         def inorder(node: TreeNode) -> list[int]:
             if not node:
                 return []
             return inorder(node.left) + [node.val] + inorder(node.right)
-        
         sorted_values = inorder(root)
         min_diff = float('inf')
         for i in range(1, len(sorted_values)):
