9673b2027b7306ac7f18da8fe8b880fbc97817ee
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2025-08-20 02:30:53 +0900

    Update 728. Self Dividing Numbers.py

--- 728. Self Dividing Numbers.py
+++ 728. Self Dividing Numbers.py
@@ -1,1 +1,25 @@
+class Solution:
+    def selfDividingNumbers(self, left: int, right: int) -> List[int]:
+        res = []
+        flag = True
 
+        for num in range(left, right + 1):
+            current_num = num
+    
+            while current_num > 0:
+                current_digit = current_num % 10
+
+                if current_digit != 0:
+                    if num % current_digit != 0:
+                        flag = False
+                else:
+                    flag = False
+
+                current_num = current_num // 10
+
+            if flag == True:
+                res.append(num)
+            else:
+                flag = True
+
+        return res
