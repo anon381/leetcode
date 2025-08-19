58f2d1ee1438fdf13a102f6daf96b411b12798a0
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2025-08-19 18:34:42 +0900

    Update 598. Range Addition II.py

--- 598. Range Addition II.py
+++ 598. Range Addition II.py
@@ -1,1 +1,8 @@
 
+class Solution:
+    def maxCount(self, m: int, n: int, ops: List[List[int]]) -> int:
+        min_x, min_y = m, n
+        for a, b in ops:
+            if min_x > a: min_x = a
+            if min_y > b: min_y = b
+        return min_x * min_y
