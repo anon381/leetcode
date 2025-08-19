92999e9c4ce61bc9fa570b80f9ee3112d1b70b86
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2025-08-19 18:29:58 +0900

    Update 2348. Number of Zero-Filled Subarrays.py

--- 2348. Number of Zero-Filled Subarrays.py
+++ 2348. Number of Zero-Filled Subarrays.py
@@ -2,3 +2,16 @@
 class Solution:
     def zeroFilledSubarray(self, nums: List[int]) -> int:
         return (Len:=0) or sum( Len:=Len+1 if x==0 else 0 for x in nums )
+## or 
+class Solution:
+    def zeroFilledSubarray(self, nums: List[int]) -> int:
+        total =0
+        count =0
+        for n in nums:
+            if n ==0:
+                count=count+1
+            else:
+                total += count * (count + 1) // 2
+                count=0
+        total += count * (count + 1) // 2
+        return total
