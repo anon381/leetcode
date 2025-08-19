30a262648db7b4d3bf410a2c04fc0e3bd17fdb57
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2025-08-19 18:35:24 +0900

    Update 598. Range Addition II.py

--- 598. Range Addition II.py
+++ 598. Range Addition II.py
@@ -6,3 +6,13 @@ class Solution:
             if min_x > a: min_x = a
             if min_y > b: min_y = b
         return min_x * min_y
+
+## or 
+class Solution:
+    def maxCount(self, m: int, n: int, ops: List[List[int]]) -> int:
+        min_row = m
+        min_col = n
+        for i in range(len(ops)):
+            min_row=min(min_row, ops[i][0])
+            min_col=min(min_col, ops[i][1])
+        return min_row*min_col
