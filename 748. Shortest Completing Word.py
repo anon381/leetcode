3c4f0d820b8ea249605f0851e0599f28f3048904
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2025-08-20 20:53:33 +0900

    Update 748. Shortest Completing Word.py

--- 748. Shortest Completing Word.py
+++ 748. Shortest Completing Word.py
@@ -1,1 +1,15 @@
+class Solution:
+    def shortestCompletingWord(self, licensePlate: str, words: List[str]) -> str:
+        words=sorted(words, key=len)
+        plate=Counter(i.lower() for i in licensePlate if i.isalpha())
 
+        for i in words:
+            if Counter(i)>=plate:
+                return i
+
+  ## or 
+
+class Solution:
+    def shortestCompletingWord(self, license_plate: str, words: List[str]) -> str:
+        letters = Counter(ltr.lower() for ltr in license_plate if ltr.isalpha())
+        return min((word for word in words if not letters - Counter(word)), key=len)
