2e191c920274e8d346e25cc538ce27e77355f394
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2025-08-20 20:45:07 +0900

    Update 482. License Key Formatting.py

--- 482. License Key Formatting.py
+++ 482. License Key Formatting.py
@@ -15,3 +15,23 @@ class Solution:
             grouping.append( S[ index : index+K ] )
 
         return '-'.join( grouping ).upper()
+        
+## or 
+
+class Solution:
+    def licenseKeyFormatting(self, s: str, k: int) -> str:
+        chars = []
+        for ch in s:
+            if ch == '-':
+                continue
+            chars.append(ch.upper())
+
+        result = []
+        count = 0
+        for ch in reversed(chars): #reverse chars
+            if count == k:
+                result.append('-')
+                count = 0
+            result.append(ch)
+            count += 1
+        return ''.join(reversed(result))
