c0b3706c7ff7672360b61e13874571389ec14884
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2025-08-21 20:50:17 +0900

    Update 922. Sort Array By Parity II.py

--- 922. Sort Array By Parity II.py
+++ 922. Sort Array By Parity II.py
@@ -1,1 +1,18 @@
 
+from typing import List
+
+class Solution:
+    def sortArrayByParityII(self, nums: List[int]) -> List[int]:
+        length = len(nums)
+        result = [0] * length
+        even_ptr, odd_ptr = 0, 1
+
+        for val in nums:
+            if val % 2 == 0:
+                result[even_ptr] = val
+                even_ptr += 2
+            else:
+                result[odd_ptr] = val
+                odd_ptr += 2
+
+        return result
