00d1e5f1ef7f1598a26df7ed7bd05267800a743f
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2025-08-21 21:20:26 +0900

    Update 904. Fruit Into Baskets.py

--- 904. Fruit Into Baskets.py
+++ 904. Fruit Into Baskets.py
@@ -22,22 +22,23 @@ class Solution:
 from collections import defaultdict
 
 class Solution:
-def totalFruit(fruits):
-    last = second_last = -1
-    last_count = curr = res = 0
+    def totalFruit(fruits):
+        last = second_last = -1
+        last_count = curr = res = 0
 
-    for f in fruits:
-        if f == last or f == second_last:
-            curr += 1
-        else:
-            curr = last_count + 1
+        for f in fruits:
+            if f == last or f == second_last:
+                curr += 1
+            else:
+                curr = last_count + 1
 
-        if f == last:
-            last_count += 1
-        else:
-            last_count = 1
-            second_last, last = last, f
+            if f == last:
+                last_count += 1
+            else:
+                last_count = 1
+                second_last, last = last, f
 
-        res = max(res, curr)
+            res = max(res, curr)
 
-    return res
+        return res
+
