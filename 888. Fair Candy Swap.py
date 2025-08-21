5367ffdb060fc4bec9c944943a70a67faf8302b7
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2025-08-21 20:42:46 +0900

    Update 888. Fair Candy Swap.py

--- 888. Fair Candy Swap.py
+++ 888. Fair Candy Swap.py
@@ -1,1 +1,19 @@
 
+class Solution:
+    def fairCandySwap(self, aliceSizes: List[int], bobSizes: List[int]) -> List[int]:
+        aliceTotal, bobTotal = 0, 0
+
+        for candy in aliceSizes:
+            aliceTotal += candy
+
+        for candy in bobSizes:
+            bobTotal += candy
+
+        delta = (bobTotal - aliceTotal) // 2
+        bobSet = set(bobSizes)
+
+        for a in aliceSizes:
+            b = a + delta
+            if b in bobSet:
+                return [a, b]
+        
