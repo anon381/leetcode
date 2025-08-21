e38ac17fb0b64956b4fae51f3a3536f843139016
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2025-08-22 03:27:33 +0900

    Update 2561. Rearranging Fruits.py

--- 2561. Rearranging Fruits.py
+++ 2561. Rearranging Fruits.py
@@ -20,3 +20,32 @@ class Solution:
         for i in range(swaps_to_make):
             total_cost += min(fruits_to_swap[i], 2 * min_val)
         return total_cost
+## or 
+from collections import Counter
+from typing import List
+
+class Solution:
+    def minCost(self, basket1: List[int], basket2: List[int]) -> int:
+        s1 = Counter(basket1)
+        s2 = Counter(basket2)
+        check = s1 + s2
+        res = 0
+        for fruit, count in check.items():
+            if count % 2 != 0:
+                return -1
+        final = []
+        excess_in_b1 = s1 - s2
+        for fruit, count in excess_in_b1.items():
+            final.extend([fruit] * (count // 2))
+        excess_in_b2 = s2 - s1
+        for fruit, count in excess_in_b2.items():
+            final.extend([fruit] * (count // 2))
+
+        final.sort()
+        min_el = min(min(basket1), min(basket2))
+        
+        for i in range(len(final) // 2):
+            swap_cost = min(2 * min_el, final[i])
+            res += swap_cost
+            
+        return res
