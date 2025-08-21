77c98b537b0a30945be117484fad2cf7cc31c8b5
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2025-08-21 21:19:51 +0900

    Update 904. Fruit Into Baskets.py

--- 904. Fruit Into Baskets.py
+++ 904. Fruit Into Baskets.py
@@ -18,3 +18,26 @@ class Solution:
             max_len = max(max_len, end - start + 1)
 
         return max_len
+## or another approach 
+from collections import defaultdict
+
+class Solution:
+def totalFruit(fruits):
+    last = second_last = -1
+    last_count = curr = res = 0
+
+    for f in fruits:
+        if f == last or f == second_last:
+            curr += 1
+        else:
+            curr = last_count + 1
+
+        if f == last:
+            last_count += 1
+        else:
+            last_count = 1
+            second_last, last = last, f
+
+        res = max(res, curr)
+
+    return res
