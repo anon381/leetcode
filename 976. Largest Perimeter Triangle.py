4e54f7076d1d4c9ea5a5026760e93e8395ab6db1
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2025-08-23 20:26:30 +0900

    Update 976. Largest Perimeter Triangle.py

--- 976. Largest Perimeter Triangle.py
+++ 976. Largest Perimeter Triangle.py
@@ -1,8 +1,27 @@
 class Solution:
     def largestPerimeter(self, nums: list[int]) -> int:
-        nums.sort(reverse=True)  # Sort in descending order
+        nums.sort(reverse=True) 
         for i in range(len(nums) - 2):
             if nums[i+1] + nums[i+2] > nums[i]:
                 return nums[i] + nums[i+1] + nums[i+2]
         return 0
+
+# in cpp
+
+# #include <bits/stdc++.h>
+# using namespace std;
+
+# class Solution {
+# public:
+#     int largestPerimeter(vector<int>& nums) {
+#         sort(nums.rbegin(), nums.rend()); // Sort in descending order
+#         for (int i = 0; i < nums.size() - 2; i++) {
+#             if (nums[i+1] + nums[i+2] > nums[i]) {
+#                 return nums[i] + nums[i+1] + nums[i+2];
+#             }
+#         }
+#         return 0;
+#     }
+# };
+
 
