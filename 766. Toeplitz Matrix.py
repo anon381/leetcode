306f410b1029d6abfa35b37715466fdae607530b
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2025-08-24 20:41:23 +0900

    Add C++ code comments for Toeplitz matrix solution
    Added C++ implementation as comments for Toeplitz matrix check.

--- 766. Toeplitz Matrix.py
+++ 766. Toeplitz Matrix.py
@@ -12,3 +12,21 @@ class Solution:
                 if row[col_i] != expected[col_i]:
                     return False
         return True
+#or in cpp
+# class Solution {
+# public:
+#        bool isToeplitzMatrix(vector<vector<int>>& matrix) {
+#     int n = matrix.size(), m = matrix[0].size();
+#     map<int, int> mp;
+#     for (int i = 0; i < n; i++) {
+#         for (int j = 0; j < m; j++) {
+#             if (mp.count(i - j) == 0)
+#                 mp[i - j] = matrix[i][j];
+#             else if (mp[i - j] != matrix[i][j])
+#                 return 0;
+#         }
+#     }
+#     return 1;
+# } 
+    
+};
