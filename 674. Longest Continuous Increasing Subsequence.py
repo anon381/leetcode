d947b8968b21d417604762ae0091749c70e04116
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2025-08-24 20:30:57 +0900

    Implement function to find length of LCIS

--- 674. Longest Continuous Increasing Subsequence.py
+++ 674. Longest Continuous Increasing Subsequence.py
@@ -1,1 +1,12 @@
 
+class Solution:
+    def findLengthOfLCIS(self, nums: List[int]) -> int:
+        count = 1
+        prev = 0
+        for i in range(1, len(nums)):
+            if nums[i] > nums[i-1]:
+                count += 1
+            else:
+                prev = max(count, prev)
+                count = 1
+        return max(prev, count)
