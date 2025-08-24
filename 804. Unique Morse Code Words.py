d2dd597a0ef3d364757e7172997074ca29321490
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2025-08-24 20:48:37 +0900

    Refactor unique Morse code words calculation

--- 804. Unique Morse Code Words.py
+++ 804. Unique Morse Code Words.py
@@ -10,3 +10,26 @@ class Solution:
                 k += morse_dict[i]
             words2.append(k) 
         return len(set(words2))
+# in cpp
+# class Solution {
+# public:
+#     int uniqueMorseRepresentations(vector<string>& words) {
+#         vector<string> morseCode = {
+#             ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---",
+#             "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-",
+#             "..-", "...-", ".--", "-..-", "-.--", "--.."
+#         };
+
+#         unordered_set<string> uniqueMorse;
+
+#         for (const string& word : words) {
+#             string morseRepresentation = "";
+#             for (char c : word) {
+#                 morseRepresentation += morseCode[c - 'a'];
+#             }
+#             uniqueMorse.insert(morseRepresentation);
+#         }
+
+#         return uniqueMorse.size();
+#     }
+# };
