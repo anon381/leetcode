1b1fa2696edce9dc7f5a68f246e92d3d93ff51e1
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2025-08-25 18:06:55 +0900

    Add C++ code comments for projection area function
    Added C++ implementation as comments for projection area calculation.

--- 883. Projection Area of 3D Shapes.py
+++ 883. Projection Area of 3D Shapes.py
@@ -5,3 +5,17 @@ class Solution:
         top = sum(v > 0 for row in grid for v in row)
         return ver + hor + top
         
+# or in cpp
+    # int projectionArea(const vector<vector<int>>& grid) {
+    #     int res = 0, n = grid.size(), x, y;
+    #     for (int i = 0; i < n; ++i) {
+    #         x = 0, y = 0;
+    #         for (int j = 0; j < n; ++j) {
+    #             x = max(x, grid[i][j]);
+    #             y = max(y, grid[j][i]);
+    #             if (grid[i][j]) ++res;
+    #         }
+    #         res += x + y;
+    #     }
+    #     return res;
+    # }
