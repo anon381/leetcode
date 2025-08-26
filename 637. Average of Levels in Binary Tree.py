f42f5d103c4faf03c4d1b7b39148b8b1ccb867ef
Yabets Maregn <yabetsanon@gmail.com>
2025-08-26 18:08:54 +0900

    Update 637. Average of Levels in Binary Tree.py

--- 637. Average of Levels in Binary Tree.py
+++ 637. Average of Levels in Binary Tree.py
@@ -1,1 +1,32 @@
 
+# or in cpp
+# class Solution {
+# public:
+#     vector<double> averageOfLevels(TreeNode* root) {
+#         vector<double> ans;
+#         queue<TreeNode*> q;
+
+#         q.push(root);
+
+#         while (!q.empty()) {
+#             int n = q.size();
+#             double sum = 0;
+
+#             for (int i = 0; i < n; ++i) {
+#                 TreeNode* curnode = q.front();
+#                 q.pop();
+
+#                 sum += curnode->val;
+
+#                 if (curnode->left)
+#                     q.push(curnode->left);
+
+#                 if (curnode->right)
+#                     q.push(curnode->right);
+#             }
+#             ans.push_back(sum / n);
+#         }
+
+#         return ans;
+#     }
+# };
