156004fff5649d29be4c5daa970b1a6c3c4b9bf4
Yabets Maregn <yabetsanon@gmail.com>
2025-08-27 01:44:53 +0900

    Include C++ code example for rotate list solution
    Added C++ implementation for rotating a linked list.

--- 61. Rotate List.py
+++ 61. Rotate List.py
@@ -30,3 +30,32 @@ class Solution:
         dummy.next = head
 
         return new_head
+# or in cpp
+# class Solution {
+# public:
+#     ListNode* rotateRight(ListNode* head, int k) {
+#         if (!head) return head;
+
+#         int length = 1;
+#         ListNode* dummy = head;
+
+#         while (dummy->next) {
+#             dummy = dummy->next;
+#             length++;
+#         }
+
+#         int position = k % length;
+#         if (position == 0) return head;
+
+#         ListNode* current = head;
+#         for (int i = 0; i < length - position - 1; ++i) {
+#             current = current->next;
+#         }
+
+#         ListNode* newHead = current->next;
+#         current->next = nullptr;
+#         dummy->next = head;
+
+#         return newHead;        
+#     }
+# };
