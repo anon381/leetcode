c155e90464f32b3197215ddb84daba480c68c42b
Yabets Maregn <yabetsanon@gmail.com>
2025-08-27 01:55:41 +0900

    Add Java solution for removing duplicates

--- 83. Remove Duplicates from Sorted List.py
+++ 83. Remove Duplicates from Sorted List.py
@@ -32,3 +32,20 @@ class Solution:
 #         return res;        
 #     }
 # };
+
+# in java 
+# class Solution {
+#     public ListNode deleteDuplicates(ListNode head) {
+#         ListNode res = head;
+
+#         while (head != null && head.next != null) {
+#             if (head.val == head.next.val) {
+#                 head.next = head.next.next;
+#             } else {
+#                 head = head.next;
+#             }
+#         }
+
+#         return res;        
+#     }
+# }
