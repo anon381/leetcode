280ed5ddc14f314f5b1346a3c0e89fcc24c22213
Yabets Maregn <yabetsanon@gmail.com>
2025-08-27 20:29:06 +0900

    Update 819. Most Common Word.py

--- 819. Most Common Word.py
+++ 819. Most Common Word.py
@@ -3,3 +3,17 @@ class Solution:
         ban = set(banned)
         words = re.findall(r'\w+', p.lower())
         return collections.Counter(w for w in words if w not in ban).most_common(1)[0][0]        
+# in cpp
+# public solution 
+#     string mostCommonWord(string p, vector<string>& banned) {
+#         unordered_set<string> ban(banned.begin(), banned.end());
+#         unordered_map<string, int> count;
+#         for (auto & c: p) c = isalpha(c) ? tolower(c) : ' ';
+#         istringstream iss(p);
+#         string w;
+#         pair<string, int> res ("", 0);
+#         while (iss >> w)
+#             if (ban.find(w) == ban.end() && ++count[w] > res.second)
+#                 res = make_pair(w, count[w]);
+#         return res.first;
+#     }
