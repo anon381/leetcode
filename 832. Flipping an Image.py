ee90b717ed22a9a0062ce2fd5a3f47098eed8388
Yabets Maregn <yabetsanon@gmail.com>
2025-08-27 20:32:44 +0900

    Update 832. Flipping an Image.py

--- 832. Flipping an Image.py
+++ 832. Flipping an Image.py
@@ -1,1 +1,12 @@
-
+class Solution(object):
+    def flipAndInvertImage(self, image):
+        for i in  image:
+            l,r=0,len(image)-1
+            while l<=r:
+                if l==r:
+                    i[l]=1-i[l]
+                else:
+                    i[l],i[r]=1-i[r],1-i[l]
+                l+=1
+                r-=1
+        return  image
