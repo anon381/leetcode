ee1af09eb2964a7071d6e4a73de0d74181072225
Yabets Maregn <yabetsanon@gmail.com>
2025-08-28 19:27:45 +0900

    Update 182. Duplicate Emails.py

--- 182. Duplicate Emails.py
+++ 182. Duplicate Emails.py
@@ -1,1 +1,10 @@
 
+import pandas as pd
+
+def duplicate_emails(person: pd.DataFrame) -> pd.DataFrame:
+
+    duplicate_rows = person.loc[person.duplicated('email') == True]
+    
+    result = duplicate_rows[['email']].drop_duplicates(keep='first')
+    
+    return result
