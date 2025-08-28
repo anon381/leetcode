1dee10ba4ab4ff40cc8d37cfdddc60fea81cf3a8
Yabets Maregn <yabetsanon@gmail.com>
2025-08-28 20:09:24 +0900

    Update 1389. Create Target Array in the Given Order.py

--- 1389. Create Target Array in the Given Order.py
+++ 1389. Create Target Array in the Given Order.py
@@ -5,3 +5,12 @@ class Solution:
         for n,i in zip(nums,index): 
             arr.insert(i,n)
         return arr
+
+        
+        #With out insert
+class Solution:
+    def createTargetArray(self, nums: List[int], index: List[int]) -> List[int]:
+        arr = []
+        for n,i in zip(nums,index): 
+            arr[i:i] = [n]
+        return arr
