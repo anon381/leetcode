4120c32998e924b633d6733e48ff52233ecaa7b2
Yabets Maregn <yabetsanon@gmail.com>
2025-08-28 20:01:19 +0900

    Update 657. Robot Return to Origin.py

--- 657. Robot Return to Origin.py
+++ 657. Robot Return to Origin.py
@@ -1,3 +1,17 @@
 class Solution:
     def judgeCircle(self, moves: str) -> bool:
         return moves.count("U")==moves.count("D") and moves.count("L")==moves.count("R")
+# in java 
+# class Solution {
+#     public boolean judgeCircle(String moves) {
+#         int x = 0, y = 0;
+
+#         for(char move: moves.toCharArray()){
+#             if(move == 'U') y++;
+#             else if(move == 'D') y--;
+#             else if(move == 'L') x--;
+#             else if(move == 'R') x++;
+#         }
+#         return x == 0 && y == 0;
+#     }
+# }
