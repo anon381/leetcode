fc2dc08c554ea586c693b9504dd12d595a0d2e9f
Yabets Maregn <yabetsanon@gmail.com>
2025-08-29 21:25:40 +0900

    Add C++ and Java examples for hasGroupsSizeX

--- 914. X of a Kind in a Deck of Cards.py
+++ 914. X of a Kind in a Deck of Cards.py
@@ -7,3 +7,24 @@ class Solution:
             return reduce(gcd, count) > 1
 
 
+#in cpp
+    # bool hasGroupsSizeX(vector<int>& deck) {
+    #     unordered_map<int, int> count;
+    #     int res = 0;
+    #     for (int i : deck) count[i]++;
+    #     for (auto i : count) res = __gcd(i.second, res);
+    #     return res > 1;
+    # }
+
+# in java 
+    # public boolean hasGroupsSizeX(int[] deck) {
+    #     Map<Integer, Integer> count = new HashMap<>();
+    #     int res = 0;
+    #     for (int i : deck) count.put(i, count.getOrDefault(i, 0) + 1);
+    #     for (int i : count.values()) res = gcd(i, res);
+    #     return res > 1;
+    # }
+
+    # public int gcd(int a, int b) {
+    #     return b > 0 ? gcd(b, a % b) : a;
+    # }
