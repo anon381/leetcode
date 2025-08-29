a8acda6f473884279b924eea21a6255a2a024ddf
Yabets Maregn <yabetsanon@gmail.com>
2025-08-29 21:25:06 +0900

    Update 914. X of a Kind in a Deck of Cards.py

--- 914. X of a Kind in a Deck of Cards.py
+++ 914. X of a Kind in a Deck of Cards.py
@@ -1,1 +1,9 @@
+class Solution:
+        def hasGroupsSizeX(self, deck):
+            def gcd(a, b):
+                while b: a, b = b, a % b
+                return a
+            count = collections.Counter(deck).values()
+            return reduce(gcd, count) > 1
+
 
