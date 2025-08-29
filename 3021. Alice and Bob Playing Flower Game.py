9020f08a12cbca757ebaa9dc8c336aebf827beda
Yabets Maregn <yabetsanon@gmail.com>
2025-08-29 21:01:34 +0900

    Include C++ code comments for flowerGame function

--- 3021. Alice and Bob Playing Flower Game.py
+++ 3021. Alice and Bob Playing Flower Game.py
@@ -1,3 +1,10 @@
 class Solution:
     def flowerGame(self, n: int, m: int) -> int:
         return n * m // 2
+# in cpp
+# class Solution {
+# public:
+#     long long flowerGame(int n, int m) {
+#         return 1LL * n * m / 2;
+#     }
+# };
