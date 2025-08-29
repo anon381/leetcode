5d7964998971ab9b78fbf2b3d6233e2921035369
Yabets Maregn <yabetsanon@gmail.com>
2025-08-29 21:09:53 +0900

    Include C++ code for surface area calculation

--- 892. Surface Area of 3D Shapes.py
+++ 892. Surface Area of 3D Shapes.py
@@ -12,3 +12,22 @@ class Solution:
                     if c > 0:
                         total_area -= 2 * min(grid[r][c], grid[r][c - 1])
         return total_area
+        
+#in cpp
+# class Solution {
+# public:
+#     int surfaceArea(vector<vector<int>>& grid) {
+#         int ans = 0;
+#         int n = grid.size();
+#         for(int i = 0; i < n; i++){
+#             for(int j = 0; j < n; j++){
+#                 if(grid[i][j]) ans += 4;
+#                 ans += i - 1 < 0 ? 2 * grid[i][j] : abs(grid[i][j] - grid[i - 1][j]);
+#                 ans += i + 1 == n ? 2 * grid[i][j] : abs(grid[i][j] - grid[i + 1][j]);
+#                 ans += j - 1 < 0 ? 2 * grid[i][j] : abs(grid[i][j] - grid[i][j - 1]);
+#                 ans += j + 1 == n ? 2 * grid[i][j] : abs(grid[i][j] - grid[i][j + 1]);
+#             }
+#         }
+#         return ans / 2;
+#     }
+# };
