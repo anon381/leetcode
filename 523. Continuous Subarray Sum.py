b81dba90f49c625b12852b5ff1153ac028e1c91c
Yabets Maregn <yabetsanon@gmail.com>
2025-08-30 19:51:53 +0900

    Add C++ solution for Continuous Subarray Sum
    Added C++ implementation of the checkSubarraySum function as a comment.

--- 523. Continuous Subarray Sum.py
+++ 523. Continuous Subarray Sum.py
@@ -10,4 +10,49 @@ class Solution:
             elif i - remainder_cache[remainder] >=2:
                 return True
         return False
+
+
+
+
         
+#in cpp
+# class Solution {
+# public:
+#     bool checkSubarraySum(vector<int>& nums, int k) {
+       
+        
+        
+#         if(nums.size()<2)
+#             return false;
+        
+        
+        
+#         mp[0]=-1;
+        
+#         int runningSum=0;
+        
+#         for(int i=0;i<nums.size();i++)
+#         {
+#             runningSum+=nums[i];
+            
+#             if(k!=0) 
+#                 runningSum = runningSum%k;
+            
+#             if(mp.find(runningSum)!=mp.end())
+#             {
+                
+#                 if(i-mp[runningSum]>1)
+#                     return true;
+#             }
+#             else
+#             {
+                
+#                 mp[runningSum]=i;
+#             }
+                    
+#         }
+        
+#         return false;
+        
+#     }
+# };
