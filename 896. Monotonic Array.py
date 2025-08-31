3b1fd3ebcc7771045526c91d9bd6093f7497a4dd
Yabets Maregn <yabetsanon@gmail.com>
2025-08-31 20:59:02 +0900

    Add Java solution for monotonic array check
    Added Java implementation of the isMonotonic function as a comment.

--- 896. Monotonic Array.py
+++ 896. Monotonic Array.py
@@ -42,6 +42,30 @@ class Solution:
 #             }
 #         }
 
+
+
+
+#in java 
+# class Solution {
+#     public boolean isMonotonic(int[] nums) {
+#         int n = nums.length;
+#         if (n == 1) return true;
+#         boolean isInc = true;
+#         boolean isDec = true;
+#         for (int i = 1; i < n; i++) {
+#             if (!isInc && !isDec) {
+#                 return false;
+#             }
+#             if (nums[i] < nums[i - 1]) {
+#                 isInc = false;
+#             }
+#             if (nums[i] > nums[i - 1]) {
+#                 isDec = false;
+#             }
+#         }
+#         return isInc || isDec;        
+#     }
+# }
 #         return isInc || isDec;        
 #     }
 # };
