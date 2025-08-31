98a8744c6ad3308253ad4d38b797df230f4c799b
Yabets Maregn <yabetsanon@gmail.com>
2025-08-31 21:02:35 +0900

    Add C++ solution comments to Unique Email Addresses
    Added C++ implementation of the unique email addresses solution as comments.

--- 929. Unique Email Addresses.py
+++ 929. Unique Email Addresses.py
@@ -7,3 +7,28 @@ class Solution:
             local = local.replace('.', '')
             unique_emails.add((local, domain))
         return len(unique_emails)
+
+
+#in cpp
+# class Solution {
+# public:
+#     int numUniqueEmails(vector<string>& emails) {
+#         set<pair<string, string>> uniqueEmails;
+        
+#         for (const string& email : emails) {
+#             size_t atPos = email.find('@');
+#             string local = email.substr(0, atPos);
+#             string domain = email.substr(atPos + 1);
+            
+#             size_t plusPos = local.find('+');
+#             if (plusPos != string::npos) {
+#                 local = local.substr(0, plusPos);
+#             }
+            
+#             local.erase(remove(local.begin(), local.end(), '.'), local.end());
+#             uniqueEmails.insert({local, domain});
+#         }
+        
+#         return uniqueEmails.size();
+#     }
+# };
