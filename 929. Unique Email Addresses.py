0881ccd2608625aef71e13be3f4154e1d2723158
Yabets Maregn <yabetsanon@gmail.com>
2025-08-31 21:03:35 +0900

    Implement unique email counting in Java
    Added Java implementation for counting unique email addresses.

--- 929. Unique Email Addresses.py
+++ 929. Unique Email Addresses.py
@@ -32,3 +32,27 @@ class Solution:
 #         return uniqueEmails.size();
 #     }
 # };
+
+
+        
+
+# in java
+
+# class Solution {
+#     public int numUniqueEmails(String[] emails) {
+#         Set<String> uniqueEmails = new HashSet<>();
+        
+#         for (String email : emails) {
+#             String[] parts = email.split("@");
+#             String local = parts[0];
+#             String domain = parts[1];
+            
+#             local = local.split("\\+")[0];
+#             local = local.replace(".", "");
+            
+#             uniqueEmails.add(local + "@" + domain);
+#         }
+        
+#         return uniqueEmails.size();
+#     }
+# }
