ab487914b7c7761264ff56b8615ce44241b1bd7f
Yabets Maregn <yabetsanon@gmail.com>
2025-09-01 09:39:14 +0900

    Add C++ code snippet for heightChecker function
    Added C++ implementation as a comment for heightChecker.

--- 1051. Height Checker.py
+++ 1051. Height Checker.py
@@ -1,3 +1,19 @@
 class Solution:
     def heightChecker(self, heights: List[int]) -> int:
         return sum(h1 != h2 for h1, h2 in zip(heights, sorted(heights)))
+
+
+#in cpp
+# class Solution {
+# public:
+#     int heightChecker(vector<int>& heights) {
+#         vector<int> v(heights);
+#         sort(v.begin(),v.end());
+#         int count = 0;
+#         for(int i=0;i<heights.size();i++){
+#             if(heights[i] != v[i])
+#              count++;
+#         }
+#         return count;
+#     }
+# };
