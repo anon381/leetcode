7a405d2f668f7d5d7261a1db31f9db81d56e8458
Yabets Maregn <yabetsanon@gmail.com>
2025-09-01 22:36:00 +0900

    Update 188. Best Time to Buy and Sell Stock IV.py

--- 188. Best Time to Buy and Sell Stock IV.py
+++ 188. Best Time to Buy and Sell Stock IV.py
@@ -10,7 +10,17 @@ class Solution:
                 dp[i][j][1] = max(dp[i-1][j][1], dp[i-1][j-1][0] - a[i])
         return dp[n-1][k][0]
 
-        
+        # or 
+
+class Solution:
+    def maxProfit(self, k: int, prices: List[int]) -> int:
+        if k==0: return 0
+        dp = [[1000,0] for _ in range(k+1)]
+        for price in prices:
+            for i in range(1,k+1):
+                dp[i][0] = min(dp[i][0], price - dp[i-1][1])
+                dp[i][1] = max(dp[i][1] , price - dp[i][0])
+        return dp[k][1]
 
 #in cpp
 # class Solution {
