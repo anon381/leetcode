772bef82e7dbd9c452c5971996e2aefa2f51d4f1
Yabets Maregn <yabetsanon@gmail.com>
2025-09-01 22:30:36 +0900

    Include C++ solution for Best Time to Buy and Sell Stock IV

--- 188. Best Time to Buy and Sell Stock IV.py
+++ 188. Best Time to Buy and Sell Stock IV.py
@@ -9,3 +9,31 @@ class Solution:
                 dp[i][j][0] = max(dp[i-1][j][0], dp[i-1][j][1] + a[i])
                 dp[i][j][1] = max(dp[i-1][j][1], dp[i-1][j-1][0] - a[i])
         return dp[n-1][k][0]
+
+        
+
+#in cpp
+# class Solution {
+# public:
+#     int maxProfit(int k, vector<int>& prices) {
+#         int n = prices.size();
+#         if (n == 0) return 0;
+#         if (k >= n / 2) {
+#             int profit = 0;
+#             for (int i = 1; i < n; i++) {
+#                 if (prices[i] > prices[i - 1]) 
+#                     profit += prices[i] - prices[i - 1];
+#             }
+#             return profit;
+#         }
+
+#         vector<int> buy(k + 1, INT_MIN), sell(k + 1, 0);
+#         for (int price : prices) {
+#             for (int t = 1; t <= k; t++) {
+#                 buy[t] = max(buy[t], sell[t - 1] - price);
+#                 sell[t] = max(sell[t], buy[t] + price); 
+#             }
+#         }
+#         return sell[k];
+#     }
+# };
