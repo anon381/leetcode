33949ee1babb2e0c931b76550aad26c6a1b6e38c
Yabets Maregn <yabetsanon@gmail.com>
2025-09-01 22:31:54 +0900

    Implement maxProfit in Java for stock problem

--- 188. Best Time to Buy and Sell Stock IV.py
+++ 188. Best Time to Buy and Sell Stock IV.py
@@ -37,3 +37,29 @@ class Solution:
 #         return sell[k];
 #     }
 # };
+
+
+
+
+# in java
+
+# in java 
+# class Solution {
+#     public int maxProfit(int k, int[] prices) {
+#         int[] buy = new int[k + 1];
+#         int[] sell = new int[k + 1];
+
+#         for (int i = 1; i <= k; i++) {
+#             buy[i] = Integer.MIN_VALUE;
+#         }
+
+#         for (int price : prices) {
+#             for (int i = 1; i <= k; i++) {
+#                 buy[i] = Math.max(buy[i], sell[i - 1] - price);
+#                 sell[i] = Math.max(sell[i], buy[i] + price);
+#             }
+#         }
+        
+#         return sell[k];
+#     }
+# }
