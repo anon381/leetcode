af0cdf90217dc42564092f800d5411a04d3863ef
Yabets Maregn <yabetsanon@gmail.com>
2025-09-01 22:29:52 +0900

    Add maxProfit method for stock trading problem

--- 188. Best Time to Buy and Sell Stock IV.py
+++ 188. Best Time to Buy and Sell Stock IV.py
@@ -1,1 +1,11 @@
-
+class Solution:
+    def maxProfit(self, k: int, a: List[int]) -> int:
+        n=len(a)
+        dp = [[[0]*2 for _ in range(k+1)] for _ in range(n)]
+        for j in range(1,k+1):
+            dp[0][j][1] = -a[0]
+        for i in range(1, n):
+            for j in range(1, k+1):
+                dp[i][j][0] = max(dp[i-1][j][0], dp[i-1][j][1] + a[i])
+                dp[i][j][1] = max(dp[i-1][j][1], dp[i-1][j-1][0] - a[i])
+        return dp[n-1][k][0]
