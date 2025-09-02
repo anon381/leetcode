17f49d0dfb5a941cde8a004076603a42b96fb296
Yabets Maregn <yabetsanon@gmail.com>
2025-09-02 18:50:14 +0900

    Update 796. Rotate String.py

--- 796. Rotate String.py
+++ 796. Rotate String.py
@@ -6,3 +6,12 @@ class Solution:
 
 
 #in cpp
+# class Solution {
+# public:
+#     bool rotateString(string s, string goal) {
+#         if (s.length() != goal.length()) {
+#             return false;
+#         }
+#         return (s + s).find(goal) != string::npos;
+#     }
+# };
