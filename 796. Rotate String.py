dc1b9d183a25c57976d31a88c5c944b1a3201df8
Yabets Maregn <yabetsanon@gmail.com>
2025-09-02 18:50:42 +0900

    Add Java solution for Rotate String problem
    Added Java implementation of the rotateString method.

--- 796. Rotate String.py
+++ 796. Rotate String.py
@@ -18,3 +18,11 @@ class Solution:
 
 
 #in java
+# class Solution {
+#     public boolean rotateString(String s, String goal) {
+#         if (s.length() != goal.length()) {
+#             return false;
+#         }
+#         return (s + s).contains(goal);
+#     }
+# }
