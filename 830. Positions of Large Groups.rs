d8253cb2ad09a9adfe1437c3af4235218323bbf2
Yabets Maregn <yabetsanon@gmail.com>
2025-09-02 18:59:42 +0900

    Implement large_group_positions function in Rust

--- 830. Positions of Large Groups.rs
+++ 830. Positions of Large Groups.rs
@@ -1,1 +1,25 @@
-
+impl Solution {
+    pub fn large_group_positions(s: String) -> Vec<Vec<i32>> {
+        s.chars()
+            .enumerate()
+            .scan(None, |state, (i, next)| {
+                *state = match *state {
+                    Some((start, prev)) if prev == next => Some((start, prev)),
+                    _ => Some((i, next)),
+                };
+                *state
+            })
+            .enumerate()
+            .map(|(end, (start, _))| (start as i32, end as i32))
+            .filter(|(start, end)| end - start >= 2)
+            .fold(Vec::new(), |mut groups, (start, end)| {
+                match groups.last_mut() {
+                    Some(group) if *group.first().unwrap() == start => {
+                        *group.last_mut().unwrap() = end;
+                    }
+                    _ => groups.push(vec![start, end]),
+                }
+                groups
+            })
+    }
+}
