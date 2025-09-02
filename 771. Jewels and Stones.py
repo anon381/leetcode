9b799f0306b61da53d56b2d01ae24ba384933525
Yabets Maregn <yabetsanon@gmail.com>
2025-09-02 18:47:51 +0900

    Add Java solution as comments in the Python file
    Added Java implementation of the Jewels and Stones problem as comments.

--- 771. Jewels and Stones.py
+++ 771. Jewels and Stones.py
@@ -31,5 +31,24 @@ class Solution:
 # };
 
 
-#in java
 
+        
+#in java
+# import java.util.HashSet;
+# import java.util.Set;
+
+# class Solution {
+#     public int numJewelsInStones(String jewels, String stones) {
+#         Set<Character> jewelSet = new HashSet<>();
+#         for (char j : jewels.toCharArray()) {
+#             jewelSet.add(j);
+#         }
+#         int count = 0;
+#         for (char s : stones.toCharArray()) {
+#             if (jewelSet.contains(s)) {
+#                 count++;
+#             }
+#         }
+#         return count;
+#     }
+# }
