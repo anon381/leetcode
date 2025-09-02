36b154d3922da77a049aa0b808805397e25f56b6
Yabets Maregn <yabetsanon@gmail.com>
2025-09-03 06:47:50 +0900

    Add distinct subsequences II solution
    Implement dynamic programming solution for counting distinct subsequences.

--- 940. Distinct Subsequences II.py
+++ 940. Distinct Subsequences II.py
@@ -1,1 +1,20 @@
+class Solution:
+    def distinctSubseqII(self, s: str) -> int:
+        n=len(s)
+        dp=[0]*(n+1)
+        dp[0]=1
+        mod=10**9+7
+        d={}
+        for i in range(1,n+1):
+            char=s[i-1]
+            
+            dp[i]=(2*dp[i-1])%mod
+            if char in d:
+                idx=d[char]
+                dp[i]=((dp[i]-dp[idx-1])+mod)%mod
+            d[char]=i
+        return (dp[n]-1)%mod
 
+
+
+        
