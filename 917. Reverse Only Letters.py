ca607ee77fa93f29f2c9a01dd24c3a956e3e41f1
Yabets Maregn <yabetsanon@gmail.com>
2025-09-03 06:33:26 +0900

    Refactor reverseOnlyLetters method
    Refactor reverseOnlyLetters method to improve readability.

--- 917. Reverse Only Letters.py
+++ 917. Reverse Only Letters.py
@@ -5,27 +5,15 @@
 
 
 class Solution(object):
-
     def reverseOnlyLetters(self, s):
-        
         arr = list(s)
-
         left, right = 0, len(s) - 1
-
         while left < right:
-
             while left < right and not arr[left].isalpha():
-
                 left += 1
-
             while left < right and not arr[right].isalpha():
-
                 right -= 1
-
             arr[left], arr[right] = arr[right], arr[left]
-
             left += 1
-
             right -= 1
-
         return ''.join(arr)
