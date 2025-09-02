b7f455c7890eaecb23d5166572e06843b199361f
Yabets Maregn <yabetsanon@gmail.com>
2025-09-03 06:40:57 +0900

    Implement C++ solution for long pressed name
    Added C++ class implementation for long pressed name check.

--- 925. Long Pressed Name.py
+++ 925. Long Pressed Name.py
@@ -24,3 +24,24 @@ class Solution:
 
 
 # in cpp
+# class Solution {
+# public:
+#     bool isLongPressedName(string name, string typed) {
+#         int i = 0, j = 0;
+
+#         while(j < typed.size()){
+#             if(i < name.size() && name[i] == typed[j]){
+#                 i++;
+#                 j++;
+#             }
+#             else if(j > 0 && typed[j] == typed[j - 1]){
+#                 j++;
+#             }
+#             else{
+#                 return false;
+#             }
+#         }
+
+#         return i == name.size();
+#     }
+# };
