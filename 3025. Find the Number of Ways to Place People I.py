c678cfbe396d13a3b1134321b944bc19af017e4d
Yabets Maregn <yabetsanon@gmail.com>
2025-09-02 18:44:53 +0900

    Remove commented Java sorting code
    Removed commented-out Java sorting code from the solution.

--- 3025. Find the Number of Ways to Place People I.py
+++ 3025. Find the Number of Ways to Place People I.py
@@ -52,7 +52,6 @@ class Solution:
 
 # class Solution {
 #     public int numberOfPairs(int[][] points) {
-#         // Sort by x ascending, y descending
 #         Arrays.sort(points, (a, b) -> {
 #             if (a[0] == b[0]) return Integer.compare(b[1], a[1]);
 #             return Integer.compare(a[0], b[0]);
