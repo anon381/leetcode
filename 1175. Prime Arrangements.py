09cf3cbcd4cc97a5d1a826f2afa8b41476afbc5d
Yabets Maregn <yabetsanon@gmail.com>
2025-09-04 14:30:34 +0900

    Add time and space complexity comments
    Added complexity analysis for the numPrimeArrangements method.

--- 1175. Prime Arrangements.py
+++ 1175. Prime Arrangements.py
@@ -1,3 +1,11 @@
+# Complexity
+# Time complexity:
+# O(n) 
+
+# Space complexity:
+# O(1) 
+
+
 class Solution:
     def numPrimeArrangements(self, n: int) -> int:
         p = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
