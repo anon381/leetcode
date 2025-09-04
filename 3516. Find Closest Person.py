612e72424fbb3b1a1ffcef85d00457204b082069
Yabets Maregn <yabetsanon@gmail.com>
2025-09-04 14:02:23 +0900

    Add C++ version of findClosest function
    Added C++ version of the findClosest function as a comment.

--- 3516. Find Closest Person.py
+++ 3516. Find Closest Person.py
@@ -2,3 +2,13 @@ class Solution:
     def findClosest(self, x: int, y: int, z: int) -> int:
         a, b = abs(x - z), abs(y - z)
         return (a != b) << (a > b)
+
+        
+# C++ version
+# class Solution {
+# public:
+#     int findClosest(int x, int y, int z) {
+#         int a = abs(x - z), b = abs(y - z);
+#         return (a != b) << (a > b);
+#     }
+# };
