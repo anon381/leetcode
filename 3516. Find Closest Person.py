3d459813234a036c2b27916cd2c53d03d7dc3114
Yabets Maregn <yabetsanon@gmail.com>
2025-09-04 14:02:58 +0900

    Add Java version of findClosest function

--- 3516. Find Closest Person.py
+++ 3516. Find Closest Person.py
@@ -12,3 +12,12 @@ class Solution:
 #         return (a != b) << (a > b);
 #     }
 # };
+
+
+# java version 
+# class Solution {
+#     public int findClosest(int x, int y, int z) {
+#         int a = Math.abs(x - z), b = Math.abs(y - z);
+#         return ((a > b) ? 2 : 0) | ((a < b) ? 1 : 0);
+#     }
+# }
