d14880151d87f8d5c51d73495d2c62010d636573
Yabets Maregn <yabetsanon@gmail.com>
2025-09-04 14:19:54 +0900

    Add C++ version of Day of the Year solution

--- 1154. Day of the Year.py
+++ 1154. Day of the Year.py
@@ -15,3 +15,25 @@ class Solution:
 
         
 #c++ version
+# #include <bits/stdc++.h>
+# using namespace std;
+
+# class Solution {
+# public:
+#     int dayOfYear(string date) {
+#         vector<int> list_year = {0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334};
+
+#         int year = stoi(date.substr(0, 4));
+#         int month = stoi(date.substr(5, 2));
+#         int day = stoi(date.substr(8, 2));
+
+#         // Leap year check
+#         if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+#             if (month > 2) {
+#                 day += 1;
+#             }
+#         }
+
+#         return list_year[month - 1] + day;
+#     }
+# };
