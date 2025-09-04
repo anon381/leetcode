5f25890865bba566699f74094d17c7292b1c59a7
Yabets Maregn <yabetsanon@gmail.com>
2025-09-04 14:25:44 +0900

    Add numPrimeArrangements method to Solution class
    Implement numPrimeArrangements method to calculate arrangements of prime numbers.

--- 1175. Prime Arrangements.py
+++ 1175. Prime Arrangements.py
@@ -1,1 +1,7 @@
-
+class Solution:
+    def numPrimeArrangements(self, n: int) -> int:
+        p = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
+          31, 37, 41, 43, 47, 53, 59, 61, 67,
+          71, 73, 79, 83, 89, 97]
+        primes = [x for x in range(2, n + 1) if x in p]
+        return (factorial(len(primes)) * factorial(n - len(primes))) % int(1e9 + 7)
