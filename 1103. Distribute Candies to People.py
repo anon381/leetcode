36fc431e254d9fcae40fb47cf71847f4cee21ce8
Yabets Maregn <yabetsanon@gmail.com>
2025-09-04 14:13:32 +0900

    Add distributeCandies method to Solution class
    Implement solution for distributing candies among people.

--- 1103. Distribute Candies to People.py
+++ 1103. Distribute Candies to People.py
@@ -1,1 +1,17 @@
+class Solution:
+    def distributeCandies(self, candies: int, num_people: int) -> List[int]:
+        ans = [0] * num_people
+        k = 0
+        while candies > 0:
+            if candies >= (k+1):
+                candies -= (k+1)
+                ans[k % num_people] += k + 1
+            else:
+                ans[k % num_people] += candies
+                candies = 0
+            k += 1
+        return ans
 
+            
+        
+        
