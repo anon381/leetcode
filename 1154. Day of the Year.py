842b1f05d6716052ed87eb1aeef3eca96d04be4d
Yabets Maregn <yabetsanon@gmail.com>
2025-09-04 14:20:59 +0900

    Add Java implementation for day of the year calculation

--- 1154. Day of the Year.py
+++ 1154. Day of the Year.py
@@ -37,3 +37,28 @@ class Solution:
 #         return list_year[month - 1] + day;
 #     }
 # };
+
+
+
+
+#in java
+class Solution {
+    public int dayOfYear(String date) {
+        int[] list_year = {0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334};
+
+        int year = Integer.parseInt(date.substring(0, 4));
+        int month = Integer.parseInt(date.substring(5, 7));
+        int day = Integer.parseInt(date.substring(8, 10));
+
+        // Leap year check
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            if (month > 2) {
+                day += 1;
+            }
+        }
+
+        return list_year[month - 1] + day;
+    }
+
+   
+}
