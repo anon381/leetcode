0f54cde4f2688286e6557ce175527c923329e321
Yabets Maregn <yabetsanon@gmail.com>
2025-09-07 01:33:30 +0900

    Add countRangeSum method with merge sort approach

--- 327. Count of Range Sum.py
+++ 327. Count of Range Sum.py
@@ -1,1 +1,25 @@
+class Solution:
+    def countRangeSum(self, nums, lower, upper):
+        prefix = [0]
+        for num in nums:
+            prefix.append(prefix[-1] + num)
+
+        def merge_sort(lo, hi):
+            if hi - lo <= 1:
+                return 0
+            mid = (lo + hi) // 2
+            count = merge_sort(lo, mid) + merge_sort(mid, hi)
+
+            j = k = mid
+            for left in prefix[lo:mid]:
+                while k < hi and prefix[k] - left < lower:
+                    k += 1
+                while j < hi and prefix[j] - left <= upper:
+                    j += 1
+                count += j - k
+
+            prefix[lo:hi] = sorted(prefix[lo:hi])
+            return count
+
+        return merge_sort(0, len(prefix))
 
