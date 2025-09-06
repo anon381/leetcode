53a7d8b5ad3abafd5b999661febd9ecb0fa5908f
Yabets Maregn <yabetsanon@gmail.com>
2025-09-06 16:06:06 +0900

    Add Java solution for removing adjacent duplicates
    Added Java implementation for removing adjacent duplicates.

--- 1047. Remove All Adjacent Duplicates In String.py
+++ 1047. Remove All Adjacent Duplicates In String.py
@@ -31,3 +31,17 @@ class Solution:
 
 
 #in java
+# class Solution {
+#         public String removeDuplicates(String S) {
+#         StringBuilder sb = new StringBuilder();
+#         for (char c : S.toCharArray()) {
+#             int size = sb.length();
+#             if (size > 0 && sb.charAt(size - 1) == c) {
+#                 sb.deleteCharAt(size - 1);
+#             } else {
+#                 sb.append(c);
+#             }
+#         }
+#         return sb.toString();
+#     }
+# }
