8e250282eac4cb9a04fb7b8593c74cb132260b29
Yabets Maregn <yabetsanon@gmail.com>
2025-09-06 16:04:30 +0900

    Add alternative method for removing adjacent duplicates
    Added an alternative implementation for removing adjacent duplicates using a list.

--- 1047. Remove All Adjacent Duplicates In String.py
+++ 1047. Remove All Adjacent Duplicates In String.py
@@ -2,3 +2,14 @@ class Solution:
   def removeDuplicates(self, S):
     return reduce(lambda s, c: s[:-1] if s[-1:] == c else s + c, S)
 
+# or
+
+class Solution:
+    def removeDuplicates(self, S):
+        res = []
+        for c in S:
+            if res and res[-1] == c:
+                res.pop()
+            else:
+                res.append(c)
+        return "".join(res)
