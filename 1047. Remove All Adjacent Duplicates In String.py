37ed8bcfb67d6b32db35fdf7ec28d3d484efac99
Yabets Maregn <yabetsanon@gmail.com>
2025-09-06 16:05:12 +0900

    Clean up code and remove C++ comments
    Removed C++ code comments and cleaned up the Python code.

--- 1047. Remove All Adjacent Duplicates In String.py
+++ 1047. Remove All Adjacent Duplicates In String.py
@@ -13,3 +13,18 @@ class Solution:
             else:
                 res.append(c)
         return "".join(res)
+
+
+#in cpp
+# class Solution {
+# public:
+#         string removeDuplicates(string S) {
+#         string res = "";
+#         for (char& c : S)
+#             if (res.size() && c == res.back())
+#                 res.pop_back();
+#             else
+#                 res.push_back(c);
+#         return res;
+#     }
+# };
