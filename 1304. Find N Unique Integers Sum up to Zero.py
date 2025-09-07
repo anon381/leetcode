9d86f02ffd9d3ff21ca6ba9c601be7e04e98b109
Yabets Maregn <yabetsanon@gmail.com>
2025-09-07 21:15:11 +0900

    Add C++ code example for sumZero function
    Added C++ implementation as a comment for the sumZero function.

--- 1304. Find N Unique Integers Sum up to Zero.py
+++ 1304. Find N Unique Integers Sum up to Zero.py
@@ -4,3 +4,13 @@ class Solution:
 
 
 #in cpp
+# class Solution {
+# public:
+#     vector<int> sumZero(int n) {
+#         vector<int> res(n);
+#         res[0] = n * (1 - n) / 2;
+#         for (int i = 1; i < n; ++i)
+#             res[i] = i;
+#         return res;
+#     }
+# };
