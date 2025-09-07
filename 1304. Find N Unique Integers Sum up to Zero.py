4196e9425f4df6b4030b5c25bb88254b9102dcc2
Yabets Maregn <yabetsanon@gmail.com>
2025-09-07 21:16:12 +0900

    Add Java solution for unique integers summing to zero
    Added Java implementation for finding N unique integers that sum to zero.

--- 1304. Find N Unique Integers Sum up to Zero.py
+++ 1304. Find N Unique Integers Sum up to Zero.py
@@ -15,4 +15,15 @@ class Solution:
 #     }
 # };
 
+
+        
 #in java
+# class Solution {
+#     public int[] sumZero(int n) {
+#         int[] res = new int[n];
+#         res[0] = n * (1 - n) / 2;
+#         for (int i = 1; i < n; ++i)
+#             res[i] = i;
+#         return res;
+#     }
+# }
