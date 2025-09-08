200dd614e71956f7fcdbfcad71ae61fc0a11daae
Yabets Maregn <yabetsanon@gmail.com>
2025-09-09 00:06:15 +0900

    c++ code changed

--- 1363. Largest Multiple of Three.py
+++ 1363. Largest Multiple of Three.py
@@ -37,3 +37,62 @@ class Solution:
         ans = "".join(sorted(ans))[::-1]
         return ans
 
+
+
+# c++ version
+# #include <bits/stdc++.h>
+# using namespace std;
+
+# class Solution {
+# public:
+#     string largestMultipleOfThree(vector<int>& digits) {
+#         vector<int> one, two, three;
+
+#         for (int d : digits) {
+#             if (d % 3 == 1) one.push_back(d);
+#             else if (d % 3 == 2) two.push_back(d);
+#             else three.push_back(d);
+#         }
+
+#         sort(one.begin(), one.end(), greater<int>());
+#         sort(two.begin(), two.end(), greater<int>());
+#         sort(three.begin(), three.end(), greater<int>());
+
+#         int total = accumulate(digits.begin(), digits.end(), 0);
+
+#         if (total % 3 == 1) {
+#             if (!one.empty()) {
+#                 one.pop_back();
+#             } else if (two.size() >= 2) {
+#                 two.pop_back();
+#                 two.pop_back();
+#             } else {
+#                 return "";
+#             }
+#         } else if (total % 3 == 2) {
+#             if (!two.empty()) {
+#                 two.pop_back();
+#             } else if (one.size() >= 2) {
+#                 one.pop_back();
+#                 one.pop_back();
+#             } else {
+#                 return "";
+#             }
+#         }
+
+#         vector<int> res;
+#         res.insert(res.end(), one.begin(), one.end());
+#         res.insert(res.end(), two.begin(), two.end());
+#         res.insert(res.end(), three.begin(), three.end());
+
+#         if (res.empty()) return "";
+
+#         sort(res.begin(), res.end(), greater<int>());
+
+#         if (res[0] == 0) return "0";  // only zeros remain
+
+#         string ans;
+#         for (int d : res) ans.push_back(d + '0');
+#         return ans;
+#     }
+# };
