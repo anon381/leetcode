acd1da3cac53d6f55a3e4041c0d773816a033b62
Yabets Maregn <yabetsanon@gmail.com>
2025-09-09 00:05:04 +0900

    code changed

--- 1363. Largest Multiple of Three.py
+++ 1363. Largest Multiple of Three.py
@@ -1,52 +1,39 @@
 class Solution:
-    def largestMultipleOfThree(self, digits: List[int]) -> str:
-        def bucket_sorted(arr):
-            c = collections.Counter(arr)
-            return [x for i in range(10) if i in c for x in [i]*c[i]]
+    def largestMultipleOfThree(self, digits: list[int]) -> str:
+        one = []
+        two = []
+        three = []
+        for i in digits:
+            if i % 3 == 1:
+                one.append(str(i))
+            elif i % 3 == 2:
+                two.append(str(i))
+            else:
+                three.append(str(i))
+        one.sort()
+        two.sort()
+        one = one[::-1]
+        two = two[::-1]
+        if sum(digits) % 3 == 1:
+            try:
+                one.pop()
+            except:
+                two.pop()
+                two.pop()
+        elif sum(digits) % 3 == 2:
+            try:
+                two.pop()
+            except:
+                one.pop()
+                one.pop()
+        ans = "".join(one)
+        ans += "".join(two)
+        ans += "".join(three)
         
-        d = collections.defaultdict(list)
-        for n in digits:
-            d[n%3] += n,
-        res, short, long = d[0], *sorted((d[1], d[2]), key=len)
-        short, long = bucket_sorted(short), bucket_sorted(long)
-        
-        if (len(long) - len(short)) % 3 > abs((len(long) % 3) - (len(short) % 3)):
-            short, long = (short, long) if len(short)%3 < len(long)%3 else (long, short)
-            res += short + long[abs(len(short)%3 - len(long)%3):]
-        else:
-            res += short + long[(len(long)-len(short))%3:]
-            
-        res = bucket_sorted(res)[::-1]
-        return '0' if res and not res[0] else ''.join(map(str, res))
+        if ans == "":
+            return ""
+        if ans[0] == "0":
+            return "0"
+        ans = "".join(sorted(ans))[::-1]
+        return ans
 
-# C++ version of the above Python code:
-#
-# #include <vector>
-# #include <string>
-# #include <algorithm>
-# using namespace std;
-# class Solution {
-# public:
-#     string largestMultipleOfThree(vector<int>& digits) {
-#         sort(digits.rbegin(), digits.rend());
-#         int sum = 0;
-#         vector<int> mod[3];
-#         for (int d : digits) {
-#             sum += d;
-#             mod[d % 3].push_back(d);
-#         }
-#         int r = sum % 3;
-#         if (r) {
-#             if (!mod[r].empty()) mod[r].pop_back();
-#             else if (mod[3 - r].size() >= 2) mod[3 - r].pop_back(), mod[3 - r].pop_back();
-#             else return "";
-#         }
-#         vector<int> res;
-#         for (int i = 0; i < 3; ++i) res.insert(res.end(), mod[i].begin(), mod[i].end());
-#         sort(res.rbegin(), res.rend());
-#         if (res.empty() || res[0] == 0) return "0";
-#         string ans;
-#         for (int d : res) ans += to_string(d);
-#         return ans;
-#     }
-# };
