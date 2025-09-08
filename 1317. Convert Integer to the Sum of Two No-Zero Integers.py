4a7e7e3602f75dff760f8d37e6e25b2fdc68c9fc
Yabets Maregn <yabetsanon@gmail.com>
2025-09-08 23:47:08 +0900

    Add C++ and Java examples to no-zero integers problem

--- 1317. Convert Integer to the Sum of Two No-Zero Integers.py
+++ 1317. Convert Integer to the Sum of Two No-Zero Integers.py
@@ -7,6 +7,7 @@ class Solution:
                 return [left, right]
         return []
 
+        
 #in cpp
 # class Solution {
 # public:
@@ -27,4 +28,17 @@ class Solution:
 #     }
 # };
 
+        
 #in java
+# class Solution {
+#     public int[] getNoZeroIntegers(int n) { 
+#         for(int i=1;i<n;i++)
+#         {
+#             int left = i;
+#             int right=n-left;
+#             if(!String.valueOf(left).contains("0")&&!String.valueOf(right).contains("0"))
+#             return new int[] {left,right};
+#         }
+#         return new int[0];
+#     }
+# }
