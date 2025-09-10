1fe725cb6906d8b5ee5bff76cdaf61c6b32b8f39
Yabets Maregn <yabetsanon@gmail.com>
2025-09-10 15:48:20 +0900

    added a java code

--- 1512. Number of Good Pairs.py
+++ 1512. Number of Good Pairs.py
@@ -27,3 +27,17 @@ class Solution:
 
 
         #Java version of the above python code:
+        #import java.util.*;
+        #class Solution {
+        #    public int numIdenticalPairs(int[] nums) {
+        #        int count = 0;
+        #        for (int i = 0; i < nums.length; i++) {
+        #            for (int j = i + 1; j < nums.length; j++) {
+        #                if (nums[i] == nums[j]) {
+        #                    count++;
+        #                }
+        #            }
+        #        }
+        #        return count;
+        #    }
+        #};
