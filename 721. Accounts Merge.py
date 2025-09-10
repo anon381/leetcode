51405467eb29e4e5835f78dc845bbb08d2c8b339
Yabets Maregn <yabetsanon@gmail.com>
2025-09-10 15:33:41 +0900

    Added a cpp code as a comment

--- 721. Accounts Merge.py
+++ 721. Accounts Merge.py
@@ -30,3 +30,54 @@ class Solution:
         
 
 # C++ version of the above Python code:
+# #include <vector>
+# #include <string>
+# #include <unordered_map>
+# #include <unordered_set>
+# using namespace std;
+#
+# class Solution {
+# public:
+#     vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
+#         unordered_map<string, unordered_set<string>> email_to_name;
+#         unordered_map<string, unordered_set<string>> graph;
+#         for (const auto& account : accounts) {
+#             const string& name = account[0];
+#             for (int i = 1; i < account.size(); ++i) {
+#                 const string& email = account[i];
+#                 email_to_name[email] = name;
+#                 graph[email];  // Ensure the email is in the graph
+#                 if (i > 1) {
+#                     graph[account[i - 1]].insert(email);
+#                     graph[email].insert(account[i - 1]);
+#                 }
+#             }
+#         }
+#
+#         unordered_set<string> visited;
+#         vector<vector<string>> merged_accounts;
+#         for (const auto& [email, name] : email_to_name) {
+#             if (!visited.count(email)) {
+#                 vector<string> merged;
+#                 dfs(email, visited, graph, merged);
+#                 sort(merged.begin(), merged.end());
+#                 merged_accounts.push_back({name});
+#                 merged_accounts.back().insert(merged_accounts.back().end(), merged.begin(), merged.end());
+#             }
+#         }
+#         return merged_accounts;
+#     }
+#
+# private:
+#     void dfs(const string& email, unordered_set<string>& visited,
+#              const unordered_map<string, unordered_set<string>>& graph,
+#              vector<string>& merged) {
+#         visited.insert(email);
+#         merged.push_back(email);
+#         for (const string& neighbor : graph.at(email)) {
+#             if (!visited.count(neighbor)) {
+#                 dfs(neighbor, visited, graph, merged);
+#             }
+#         }
+#     }
+# };
