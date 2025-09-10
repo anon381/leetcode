a5a11ff6b5ff3630fe7212a1fb6a3bcc30018e8e
Yabets Maregn <yabetsanon@gmail.com>
2025-09-10 15:11:22 +0900

    Added a  cpp code

--- 1733. Minimum Number of People to Teach.py
+++ 1733. Minimum Number of People to Teach.py
@@ -24,3 +24,38 @@ class Solution:
         
 
 # C++ version of the above Python code:
+# #include <vector>
+# #include <set>
+# using namespace std;
+#
+# class Solution {
+# public:
+#     int minimumTeachings(int n, vector<vector<int>>& languages, vector<vector<int>>& friendships) {
+#         int m = languages.size();
+#         vector<set<int>> know(m);
+#         for (int i = 0; i < m; i++) {
+#             know[i] = set<int>(languages[i].begin(), languages[i].end());
+#         }
+#
+#         set<int> need;
+#         for (auto& f : friendships) {
+#             int a = f[0] - 1, b = f[1] - 1;
+#             if (!know[a].empty() && !know[b].empty() && !set_intersection(know[a].begin(), know[a].end(), know[b].begin(), know[b].end())) {
+#                 need.insert(a);
+#                 need.insert(b);
+#             }
+#         }
+#
+#         if (need.empty()) return 0;
+#
+#         int ans = INT_MAX;
+#         for (int lang = 1; lang <= n; lang++) {
+#             int cnt = 0;
+#             for (int i : need) {
+#                 if (know[i].find(lang) == know[i].end()) cnt++;
+#             }
+#             ans = min(ans, cnt);
+#         }
+#         return ans;
+#     }
+# };
