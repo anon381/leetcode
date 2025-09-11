da95450f4b97552ab0ea9943f5d479b5d7f28a18
Yabets Maregn <yabetsanon@gmail.com>
2025-09-11 11:23:18 +0900

    Implement Java solution for sorting vowels

--- 2785. Sort Vowels in a String.py
+++ 2785. Sort Vowels in a String.py
@@ -55,3 +55,27 @@ class Solution:
 
 
 #in java
+# class Solution {
+#     public String sortVowels(String s) {
+#         List<Character> vowels = new ArrayList<>();
+#         for (char c : s.toCharArray()) {
+#             if ("AEIOUaeiou".indexOf(c) != -1) {
+#                 vowels.add(c);
+#             }
+#         }
+
+#         Collections.sort(vowels);
+
+#         StringBuilder result = new StringBuilder();
+#         int vIndex = 0;
+#         for (char c : s.toCharArray()) {
+#             if ("AEIOUaeiou".indexOf(c) != -1) {
+#                 result.append(vowels.get(vIndex++));
+#             } else {
+#                 result.append(c);
+#             }
+#         }
+
+#         return result.toString();
+#     }
+# }
