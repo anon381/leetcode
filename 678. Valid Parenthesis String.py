a7ca48e2e912f4d28e30e86c452f993531f3df29
Yabets Maregn <yabetsanon@gmail.com>
2025-09-12 02:52:44 +0900

    Include C++ code for valid parenthesis string solution

--- 678. Valid Parenthesis String.py
+++ 678. Valid Parenthesis String.py
@@ -14,3 +14,28 @@ class Solution:
             if leftMin < 0:
                 leftMin = 0
         return leftMin == 0
+
+#in cpp
+# class Solution {
+# public:
+#     bool checkValidString(string s) {
+#         int leftMin = 0, leftMax = 0;
+
+#         for (char c : s) {
+#             if (c == '(') {
+#                 leftMin++;
+#                 leftMax++;
+#             } else if (c == ')') {
+#                 leftMin--;
+#                 leftMax--;
+#             } else {
+#                 leftMin--;
+#                 leftMax++;
+#             }
+#             if (leftMax < 0) return false;
+#             if (leftMin < 0) leftMin = 0;
+#         }
+        
+#         return leftMin == 0;
+#     }
+# };
