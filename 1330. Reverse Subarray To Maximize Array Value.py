bf1d7a4541c1887a233011b22b464aa9bf4cdb06
Yabets Maregn <yabetsanon@gmail.com>
2025-09-12 04:04:00 +0900

    Add maxValueAfterReverse method in Solution class

--- 1330. Reverse Subarray To Maximize Array Value.py
+++ 1330. Reverse Subarray To Maximize Array Value.py
@@ -1,1 +1,14 @@
-
+class Solution:
+    def maxValueAfterReverse(self, nums: List[int]) -> int:
+        
+        maxMin = -inf
+        minMin = inf
+        S = 0
+        boundary = -inf
+        for a,b in zip(nums[:-1], nums[1:]):
+            distance = abs(a-b)
+            boundary = max(boundary, max(abs(nums[-1]-a),abs(nums[0]-b))-distance)
+            S += distance
+            minMin = min(max(a,b), minMin)
+            maxMin = max(min(a,b), maxMin)
+        return S + max(2*(maxMin-minMin),boundary)
