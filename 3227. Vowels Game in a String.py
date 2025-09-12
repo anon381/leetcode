c3a3ca3ffaff78aef352a3186b3487c34688b085
Yabets Maregn <yabetsanon@gmail.com>
2025-09-12 20:36:59 +0900

    Include C++ version of doesAliceWin in comments
    Added C++ implementation as comments for doesAliceWin method.

--- 3227. Vowels Game in a String.py
+++ 3227. Vowels Game in a String.py
@@ -2,3 +2,10 @@ class Solution:
     def doesAliceWin(self, s: str) -> bool:
         return any(((0x208222>>(ord(c)&31))&1) for c in s)
         
+#in cpp
+# class Solution {
+# public:
+#     static bool doesAliceWin(string& s) {
+#         return any_of(s.begin(), s.end(), [](char c) { return ((0x208222>>(c & 31)) & 1); });
+#     }
+# };
