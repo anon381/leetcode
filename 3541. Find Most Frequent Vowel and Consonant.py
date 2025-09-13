c4fcfec4810eefdecf071df64908f1e5c080e809
Yabets Maregn <yabetsanon@gmail.com>
2025-09-13 20:00:30 +0900

    Implement C++ version of maxFreqSum function
    Added C++ implementation of maxFreqSum function.

--- 3541. Find Most Frequent Vowel and Consonant.py
+++ 3541. Find Most Frequent Vowel and Consonant.py
@@ -9,3 +9,21 @@ class Solution:
             else:
                 con = max(con, s.count(i))
         return con+vow 
+
+
+#in cpp
+# class Solution {
+# public:
+#     int maxFreqSum(std::string s) {
+#         std::unordered_map<char, int> freq;
+#         int con = 0, vow = 0;
+#         for (char c : s) freq[c]++;
+#         for (auto& [ch, count] : freq) {
+#             if (std::string("aeiou").find(ch) != std::string::npos)
+#                 vow = std::max(vow, count);
+#             else
+#                 con = std::max(con, count);
+#         }
+#         return con + vow;
+#     }
+# };
