66e5aa17e39ef83315217e467110812e632bb3f2
Yabets Maregn <yabetsanon@gmail.com>
2025-09-13 21:43:54 +0900

    Update 1742. Maximum Number of Balls in a Box.py

--- 1742. Maximum Number of Balls in a Box.py
+++ 1742. Maximum Number of Balls in a Box.py
@@ -1,1 +1,12 @@
 
+class Solution:
+    def countBalls(self, lowLimit: int, highLimit: int) -> int:
+        d = defaultdict(int)
+        for x in range(lowLimit, highLimit + 1):
+            count = 0
+            while x:
+                count += x % 10
+                x //= 10
+            d[count] += 1
+
+        return max(d.values())
