9c57aabb261a1ee4776dd00d3966c72b2ca52d4e
Yabets Maregn <yabetsanon@gmail.com>
2025-09-14 18:16:07 +0900

    Add C++ implementation comment to Python file
    Added C++ code snippet as a comment for reference.

--- 1359. Count All Valid Pickup and Delivery Options.py
+++ 1359. Count All Valid Pickup and Delivery Options.py
@@ -4,3 +4,12 @@ class Solution:
         pickup_permutation = math.factorial(n) % cap
         delivery_permutation = reduce(mul, range(1, 2*n, 2), 1) % cap
         return pickup_permutation * delivery_permutation % cap
+
+
+#in cpp
+# int countOrders(int n) {
+#     long long res = 1, cap = 1000000007;
+#     for (int i=1; i<n+1; ++i) res = res * i % cap;
+#     for (int i=1; i<2*n; i+=2) res = res * i % cap;
+#     return res;
+# }
