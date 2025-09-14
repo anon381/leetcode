2f5fbb61abf9021bd9ce61dc2640780c2e860dc8
Yabets Maregn <yabetsanon@gmail.com>
2025-09-14 18:13:51 +0900

    Add subtractProductAndSum method to Solution class
    Implement a method to subtract the sum of digits from the product of digits for a given integer.

--- 1281. Subtract the Product and Sum of Digits of an Integer.py
+++ 1281. Subtract the Product and Sum of Digits of an Integer.py
@@ -1,1 +1,29 @@
 
+class Solution:
+    def subtractProductAndSum(self, n: int) -> int:
+        prod=1
+        sum=0
+        for i in str(n):
+            prod*=int(i)
+            sum+=int(i)
+        return prod-sum
+
+
+
+# in cpp
+# class Solution {
+# public:
+#     int subtractProductAndSum(int n) {
+#         int dsum = 0;
+#         int dproduct = 1;
+
+#         while (n > 0) {
+#             int digit = n % 10;
+#             dsum += digit;
+#             dproduct *= digit;
+#             n = n / 10;
+#         }
+
+#         return dproduct - dsum;
+#     }
+# };
