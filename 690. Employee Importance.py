2d6d0747eb71251e556b2d9a3523ab11538701e9
Yabets Maregn <yabetsanon@gmail.com>
2025-09-16 04:20:30 +0900

    Add C++ code comments for Employee Importance solution
    Added C++ implementation as comments for reference.

--- 690. Employee Importance.py
+++ 690. Employee Importance.py
@@ -19,3 +19,19 @@ class Solution:
         
         return dfs(id)
         
+#in cpp
+# class Solution {
+# public:
+#     int getImportance(vector<Employee*> employees, int id) {
+#         unordered_map<int, Employee*>m;
+#         for(auto x: employees) m[x->id] = x;
+#         int sum = 0;
+#         DFS(m, id, sum);
+#         return sum;
+#     }
+    
+#     void DFS(unordered_map<int, Employee*>& m, int id, int& sum){
+#         sum += m[id]->importance;
+#         for(auto x: m[id]->subordinates) DFS(m, x, sum);
+#     }
+# };
