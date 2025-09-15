7355adb0c83ae934028e4f4d30d1d2b6ddef3a04
Yabets Maregn <yabetsanon@gmail.com>
2025-09-16 04:58:45 +0900

    Update 1002. Find Common Characters.py

--- 1002. Find Common Characters.py
+++ 1002. Find Common Characters.py
@@ -1,13 +1,7 @@
 class Solution:
     def commonChars(self, words: List[str]) -> List[str]:
-        if len(words) < 2:
-            return words
-        res = []
-        word1 = set(words[0])
-        for char in word1:
-            frequency = min([word.count(char) for word in words])
-            res += [char] * frequency
-        return res
+        common_counts = reduce(lambda x, y: x & y, map(Counter, words))
+        return list(common_counts.elements())
 
 
 #in cpp
