779ef6f7aec8e6e6d3a55d27e4918a6c44f9a8da
Yabets Maregn <yabetsanon@gmail.com>
2025-09-17 16:51:05 +0900

    Add Java implementation as comments in Python file

--- 1588. Sum of All Odd Length Subarrays.py
+++ 1588. Sum of All Odd Length Subarrays.py
@@ -16,3 +16,10 @@
     #     return res;
     # }
 #in java
+    # public int sumOddLengthSubarrays(int[] A) {
+    #     int res = 0, n = A.length;
+    #     for (int i = 0; i < n; ++i) {
+    #         res += ((i + 1) * (n - i) + 1) / 2 * A[i];
+    #     }
+    #     return res;
+    # }
