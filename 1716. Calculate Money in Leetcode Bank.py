7ce2c8f0412bfb8d4d6d53da64626bad36cfa326
Yabets Maregn <yabetsanon@gmail.com>
2025-09-17 17:11:17 +0900

    Implement totalMoney function for calculating savings

--- 1716. Calculate Money in Leetcode Bank.py
+++ 1716. Calculate Money in Leetcode Bank.py
@@ -1,1 +1,7 @@
-
+class Solution:
+    def totalMoney(self, n: int) -> int:
+        weeks = n // 7
+        days = n % 7
+        sum_of_weeks = 28*weeks + 7*(weeks*(weeks-1)//2)
+        sum_of_days = days * (2*weeks + days + 1) // 2
+        return sum_of_weeks + sum_of_days
