caf32340951146e448071338be62a11dc27732d7
Yabets Maregn <yabetsanon@gmail.com>
2025-09-17 16:49:51 +0900

    Add solution for sum of all odd length subarrays

--- 1588. Sum of All Odd Length Subarrays.py
+++ 1588. Sum of All Odd Length Subarrays.py
@@ -1,1 +1,9 @@
-
+  class Solution(object):
+    def sumOddLengthSubarrays(self, A):
+        n = len(A)
+        res = 0
+        for l in xrange(1, n + 1, 2):
+            for i in xrange(n - l + 1):
+                res += sum(A[i:i + l])
+        return res
+        
