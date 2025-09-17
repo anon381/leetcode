9004ff3dad40968ffb0761a359332dc6ed38b5d6
Yabets Maregn <yabetsanon@gmail.com>
2025-09-17 17:18:45 +0900

    Implement solution to determine chessboard square color

--- 1812. Determine Color of a Chessboard Square.py
+++ 1812. Determine Color of a Chessboard Square.py
@@ -1,1 +1,8 @@
-
+class Solution:
+    def squareIsWhite(self, coordinates: str) -> bool:
+        letter = coordinates[0]
+        num = coordinates[1]
+        if ord(letter) % 2 == 0:
+            return int(num) % 2 != 0
+        else:
+            return int(num) % 2 == 0
