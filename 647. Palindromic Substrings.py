d4f44dea9012443d252cdda4dd8be7e8f0dee301
Yabets Maregn <yabetsanon@gmail.com>
2025-09-20 01:58:43 +0900

    Implement C++ solution for palindromic substrings
    Added C++ implementation for counting palindromic substrings.

--- 647. Palindromic Substrings.py
+++ 647. Palindromic Substrings.py
@@ -22,3 +22,28 @@ class Solution:
         return res
 
 #in cpp
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int res = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            res += count_palindrome(s, i, i);
+            res += count_palindrome(s, i, i + 1);
+        }
+
+        return res;        
+    }
+
+private:
+    int count_palindrome(string s, int left, int right) {
+        int count = 0;
+        while (left >= 0 && right < s.length() && s[left] == s[right]) {
+            count++;
+            left--;
+            right++;
+        }
+        return count;
+    }
+
+};
