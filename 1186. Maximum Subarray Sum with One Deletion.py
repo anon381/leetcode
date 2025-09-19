91be0fd3cbdd49240b402c30ab1830e1e6f8d5b9
Yabets Maregn <yabetsanon@gmail.com>
2025-09-20 03:39:03 +0900

    Update 1186. Maximum Subarray Sum with One Deletion.py

--- 1186. Maximum Subarray Sum with One Deletion.py
+++ 1186. Maximum Subarray Sum with One Deletion.py
@@ -19,4 +19,26 @@ class Solution:
 
 
 
+
 #in cpp
+class Solution {
+public:
+    int maximumSum(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> dp0(n, 0); //Max subarray sum till index i
+        vector<int> dp1(n, 0); //Max subarray sum till index i if one element deleted
+
+        dp0[0] = arr[0];
+        dp1[0] = -1e9;
+
+        int res = arr[0];
+        for(int i = 1; i < n; i++){
+            dp0[i] = max(dp0[i-1] + arr[i], arr[i]);
+
+            dp1[i] = max(dp1[i-1] + arr[i], dp0[i-1]);
+            res = max(res, max(dp0[i], dp1[i]));
+        }
+        return res;
+
+    }
+};
