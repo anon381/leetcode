914ae27fa961d18084d18c9d38c97862dd61576f
Yabets Maregn <yabetsanon@gmail.com>
2025-09-20 02:04:52 +0900

    Update 647. Palindromic Substrings.py

--- 647. Palindromic Substrings.py
+++ 647. Palindromic Substrings.py
@@ -74,4 +74,24 @@ class Solution {
 }
 
 
-#in js
+#in javaScipt
+var countSubstrings = function(s) {
+    let res = 0;
+
+    function count_palindrome(s, left, right) {
+        let count = 0;
+        while (left >= 0 && right < s.length && s[left] === s[right]) {
+            count++;
+            left--;
+            right++;
+        }
+        return count;
+    }
+
+    for (let i = 0; i < s.length; i++) {
+        res += count_palindrome(s, i, i);
+        res += count_palindrome(s, i, i + 1);
+    }
+
+    return res;    
+};
