8ada4fca959e9a20460ab651ea9987201eac6a85
Yabets Maregn <yabetsanon@gmail.com>
2025-09-19 19:41:58 +0900

    Implement Java solution for binary prefix divisibility

--- 1018. Binary Prefix Divisible By 5.py
+++ 1018. Binary Prefix Divisible By 5.py
@@ -27,3 +27,18 @@ class Solution:
 # };
 
 #in java
+# import java.util.*;
+
+# class Solution {
+#     public List<Boolean> prefixesDivBy5(int[] nums) {
+#         List<Boolean> res = new ArrayList<>();
+#         int num = 0;
+        
+#         for (int n : nums) {
+#             num = (num * 2 + n) % 5;  // keep remainder only
+#             res.add(num == 0);
+#         }
+        
+#         return res;
+#     }
+# }
