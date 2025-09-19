2f28750de6eaff32d334f87e06117f93b1e9d47b
Yabets Maregn <yabetsanon@gmail.com>
2025-09-20 02:14:37 +0900

    Update 646. Maximum Length of Pair Chain.py

--- 646. Maximum Length of Pair Chain.py
+++ 646. Maximum Length of Pair Chain.py
@@ -21,10 +21,8 @@ public:
         std::sort(pairs.begin(), pairs.end(), [](const std::vector<int>& a, const std::vector<int>& b) {
             return a[1] < b[1];
         });
-
         std::vector<int> prev = pairs[0];
         int res = 1;
-
         for (int i = 1; i < pairs.size(); i++) {
             const std::vector<int>& cur = pairs[i];
             if (cur[0] > prev[1]) {
@@ -37,4 +35,23 @@ public:
     }
 };
 
+
 #in java
+class Solution {
+    public int findLongestChain(int[][] pairs) {
+        Arrays.sort(pairs, Comparator.comparingInt(a -> a[1]));
+
+        int[] prev = pairs[0];
+        int res = 1;
+
+        for (int i = 1; i < pairs.length; i++) {
+            int[] cur = pairs[i];
+            if (cur[0] > prev[1]) {
+                res++;
+                prev = cur;
+            }
+        }
+
+        return res;        
+    }
+}
