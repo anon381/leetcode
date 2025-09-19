adf656658002d6ec53d8dad3cac154d3782c3f7f
Yabets Maregn <yabetsanon@gmail.com>
2025-09-20 03:04:09 +0900

    Update 698. Partition to K Equal Sum Subsets.py

--- 698. Partition to K Equal Sum Subsets.py
+++ 698. Partition to K Equal Sum Subsets.py
@@ -33,4 +33,37 @@ class Solution:
         return backtrack(0, 0, 0)
 
 
+
+
 #in cpp
+class Solution {
+public:
+    int len;
+    vector<int> nums;
+    vector<bool> st;
+
+    bool dfs(int start, int cur, int k) {
+        if (!k) return true;
+        if (cur == len) return dfs(0, 0, k - 1);
+        for (int i = start; i < nums.size(); i ++ ) {
+            if (st[i]) continue;
+            if (cur + nums[i] <= len) {
+                st[i] = true;
+                if (dfs(i + 1, cur + nums[i], k)) return true;
+                st[i] = false;
+            }
+            while (i + 1 < nums.size() && nums[i + 1] == nums[i]) i ++ ;
+            if (!cur || cur + nums[i] == len) return false;
+        }
+        return false;
+    }
+    bool canPartitionKSubsets(vector<int>& _nums, int k) {
+        nums = _nums;
+        st.resize(nums.size());
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        if (sum % k) return false;
+        len = sum / k;
+        sort(nums.begin(), nums.end(), greater<int>());
+        return dfs(0, 0, k);
+    }
+};
