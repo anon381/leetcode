c98ee9991dc9e4604dafb34571d108fe6b91bbb4
Yabets Maregn <yabetsanon@gmail.com>
2025-09-20 03:36:57 +0900

    Update 1186. Maximum Subarray Sum with One Deletion.py

--- 1186. Maximum Subarray Sum with One Deletion.py
+++ 1186. Maximum Subarray Sum with One Deletion.py
@@ -3,6 +3,11 @@ class Solution:
         n = len(arr)
         dp0 = [0] * n  
         dp1 = [0] * n  
-
         dp0[0] = arr[0]
         dp1[0] = -10**9  
+        res = arr[0]
+        for i in range(1, n):
+            dp0[i] = max(dp0[i-1] + arr[i], arr[i])
+            dp1[i] = max(dp1[i-1] + arr[i], dp0[i-1])
+            res = max(res, max(dp0[i], dp1[i]))
+        return res
