c042d2fb2458537cb21b6fcdd71eadb03da16f06
Yabets Maregn <yabetsanon@gmail.com>
2025-09-20 03:36:06 +0900

    Set up dp arrays for maximum subarray sum calculation

--- 1186. Maximum Subarray Sum with One Deletion.py
+++ 1186. Maximum Subarray Sum with One Deletion.py
@@ -1,1 +1,8 @@
+class Solution:
+    def maximumSum(self, arr: List[int]) -> int:
+        n = len(arr)
+        dp0 = [0] * n  
+        dp1 = [0] * n  
 
+        dp0[0] = arr[0]
+        dp1[0] = -10**9  
