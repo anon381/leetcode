6c8bdced7a21ea2f5bcfd696493c1370431538e2
Yabets Maregn <yabetsanon@gmail.com>
2025-09-20 03:24:59 +0900

    Update 712. Minimum ASCII Delete Sum for Two Strings.py

--- 712. Minimum ASCII Delete Sum for Two Strings.py
+++ 712. Minimum ASCII Delete Sum for Two Strings.py
@@ -32,11 +32,9 @@ public:
         int m = s1.length();
         int n = s2.length();
         vector<int> dp(n + 1, 0);
-
         for (int j = 1; j <= n; j++) {
             dp[j] = dp[j - 1] + s2[j - 1];
         }
-
         for (int i = 1; i <= m; i++) {
             int prev = dp[0];
             dp[0] += s1[i - 1];
@@ -53,7 +51,9 @@ public:
                 prev = temp;
             }
         }
-
         return dp[n];        
     }
 };
+
+
+#in java
