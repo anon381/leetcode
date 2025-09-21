4c87e8a2cb02997776ddd0611e5487b6b167cf1c
Yabets Maregn <yabetsanon@gmail.com>
2025-09-22 02:43:15 +0900

    Implement duplicate zeros in C++
    Added C++ implementation for duplicating zeros.

--- 1089. Duplicate Zeros.py
+++ 1089. Duplicate Zeros.py
@@ -18,3 +18,25 @@ class Solution:
                     arr[i + zeroes] = 0
 
 #in cpp
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
+        int n = arr.size();
+        int zeroes = count(arr.begin(), arr.end(), 0);
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (i + zeroes < n) {
+                arr[i + zeroes] = arr[i];
+            }
+            if (arr[i] == 0) {
+                zeroes--;
+                if (i + zeroes < n) {
+                    arr[i + zeroes] = 0;
+                }
+            }
+        }
+    }
+};
