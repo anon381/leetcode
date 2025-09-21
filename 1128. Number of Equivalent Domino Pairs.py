3aeb4931de478a2b3b9fe5adc95f450bdd09e205
Yabets Maregn <yabetsanon@gmail.com>
2025-09-22 02:49:37 +0900

    Update 1128. Number of Equivalent Domino Pairs.py

--- 1128. Number of Equivalent Domino Pairs.py
+++ 1128. Number of Equivalent Domino Pairs.py
@@ -32,3 +32,14 @@ public:
 
 
 #in java
+class Solution {
+    public int numEquivDominoPairs(int[][] dominoes) {
+        int[] count = new int[100];
+        int res = 0;
+        for (int[] d : dominoes) {
+            int val = d[0] < d[1] ? d[0] * 10 + d[1] : d[1] * 10 + d[0];
+            res += count[val]++;
+        }
+        return res;
+    }
+}
