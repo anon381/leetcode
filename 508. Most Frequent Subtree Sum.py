1429f89310dbbdb7b538648ae56f066e43a76b07
Yabets Maregn <yabetsanon@gmail.com>
2025-09-24 05:20:05 +0900

    Implement C++ version of most frequent subtree sum
    Added C++ implementation for finding the most frequent subtree sum.

--- 508. Most Frequent Subtree Sum.py
+++ 508. Most Frequent Subtree Sum.py
@@ -14,5 +14,24 @@ class Solution:
         return [s for s in count if count[s] == maxCount]
 
 
+    
 
 #in cpp
+    unordered_map<int, int> count;
+    int maxCount = 0;
+    vector<int> findFrequentTreeSum(TreeNode* root) {
+        dfs(root);
+        vector<int> res;
+        for (auto & e: count)
+            if (e.second == maxCount)
+                res.push_back(e.first);
+        return res;
+    }
+
+
+    int dfs(TreeNode* root) {
+        if (root == NULL) return 0;
+        int s = dfs(root->left) + dfs(root->right) + root->val;
+        maxCount = max(maxCount, ++count[s]);
+        return s;
+    }
