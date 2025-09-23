a56d9bb6d1457808487f0b27f3f3dc1182d37b54
Yabets Maregn <yabetsanon@gmail.com>
2025-09-24 04:59:58 +0900

    Implement palindrome pairs in C++
    Added C++ implementation of palindrome pairs algorithm.

--- 336. Palindrome Pairs.py
+++ 336. Palindrome Pairs.py
@@ -22,4 +22,61 @@ class Solution:
                     
         return res
 
+
+
 #in cpp
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> palindromePairs(vector<string>& words) {
+        unordered_map<string, int> backward;
+        vector<vector<int>> res;
+
+        // Store reversed words
+        for (int i = 0; i < words.size(); i++) {
+            string rev = words[i];
+            reverse(rev.begin(), rev.end());
+            backward[rev] = i;
+        }
+
+        for (int i = 0; i < words.size(); i++) {
+            string word = words[i];
+
+            // Case 1: Direct reverse match
+            if (backward.count(word) && backward[word] != i) {
+                res.push_back({i, backward[word]});
+            }
+
+            // Case 2: Empty string + palindrome word
+            if (!word.empty() && backward.count("") && isPalindrome(word)) {
+                res.push_back({i, backward[""]});
+                res.push_back({backward[""], i});
+            }
+
+            // Case 3: Prefix/suffix palindrome splits
+            for (int j = 0; j < word.size(); j++) {
+                string suffix = word.substr(j);
+                string prefix = word.substr(0, j);
+
+                if (backward.count(suffix) && isPalindrome(prefix)) {
+                    res.push_back({backward[suffix], i});
+                }
+                if (backward.count(prefix) && isPalindrome(suffix)) {
+                    res.push_back({i, backward[prefix]});
+                }
+            }
+        }
+        return res;
+    }
+
+private:
+    bool isPalindrome(const string& s) {
+        int l = 0, r = s.size() - 1;
+        while (l < r) {
+            if (s[l++] != s[r--]) return false;
+        }
+        return true;
+    }
+};
