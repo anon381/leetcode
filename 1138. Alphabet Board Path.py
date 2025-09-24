1926b5648e6f96062c752e5c8c89177cdf91dc3c
Yabets Maregn <yabetsanon@gmail.com>
2025-09-25 00:45:34 +0900

    Update 1138. Alphabet Board Path.py

--- 1138. Alphabet Board Path.py
+++ 1138. Alphabet Board Path.py
@@ -52,3 +52,37 @@ class Solution:
 
 
 #in cpp
+class Solution {
+public:
+
+    string alphabetBoardPath(string s) {
+        map<char,pair<int,int>> m;
+        char ch = 'a';
+        for(int i =0;i<6;i++){
+            for(int j =0;j<5;j++){
+                m[ch]={i,j};
+                if(ch=='z'){break;}
+                ch=char(ch+1);
+            }
+        }
+        int p =0,q=0;string ans;
+        while(s.size()>0){
+            char ch = s[0];int a=p-m[ch].first,b=q-m[ch].second;
+            if(ch=='z'){
+            for(int i =0;i<abs(b);i++)
+            {if(b>0){ans.push_back('L');}else{ans.push_back('R');}}
+            for(int i =0;i<abs(a);i++)
+            {if(a>0){ans.push_back('U');}else{ans.push_back('D');}}
+            }
+            else{
+            for(int i =0;i<abs(a);i++)
+            {if(a>0){ans.push_back('U');}else{ans.push_back('D');}}
+            for(int i =0;i<abs(b);i++)
+            {if(b>0){ans.push_back('L');}else{ans.push_back('R');}}
+            }ans.push_back('!');
+            s=s.substr(1,s.size()-1);
+            p=m[ch].first;q=m[ch].second;
+        }
+        return ans;
+    }
+};
