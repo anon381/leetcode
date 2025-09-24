3324f28b81e2d76c6cb4dd95b3ae25cd7edc0cbb
Yabets Maregn <yabetsanon@gmail.com>
2025-09-24 23:28:59 +0900

    Update 1110. Delete Nodes And Return Forest.py

--- 1110. Delete Nodes And Return Forest.py
+++ 1110. Delete Nodes And Return Forest.py
@@ -1,6 +1,8 @@
-Complexity Analysis
-Time complexity: O(n)
- Space complexity: O(n)
+# Complexity Analysis
+# Time complexity: O(n)
+#  Space complexity: O(n)
+
+
 class Solution:
     def delNodes(self, root: Optional[TreeNode], to_delete: list[int]) -> list[TreeNode]:
         res: dict[int, TreeNode] = {root.val: root}
