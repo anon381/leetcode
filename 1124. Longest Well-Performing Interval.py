50f89ab92147f1c8dae9c3c5105da7bd3e9c8f20
Yabets Maregn <yabetsanon@gmail.com>
2025-09-25 00:48:08 +0900

    Update 1124. Longest Well-Performing Interval.py

--- 1124. Longest Well-Performing Interval.py
+++ 1124. Longest Well-Performing Interval.py
@@ -12,3 +12,18 @@ class Solution:
         return res
 
 #in cpp
+class Solution {
+public:
+    int longestWPI(vector<int>& hours) {
+        unordered_map<int, int> seen;
+        int score = 0, res = 0;
+        for (int i = 0, n = hours.size(); i < n; ++i) {
+            if(hours[i] > 8) ++score;
+            else --score;
+            if (score > 0) res = i + 1;
+            else if (seen.count(score - 1)) res = max(res, i - seen[score - 1]);
+            if (!seen.count(score)) seen[score] = i;
+        }
+        return res;
+    }
+};
