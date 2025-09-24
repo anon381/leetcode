11d48baa7b2c6b2ff0c2effa4b03c9d31a30c3c7
Yabets Maregn <yabetsanon@gmail.com>
2025-09-24 23:27:55 +0900

    Update 1110. Delete Nodes And Return Forest.py

--- 1110. Delete Nodes And Return Forest.py
+++ 1110. Delete Nodes And Return Forest.py
@@ -127,3 +127,38 @@ class Solution {
 
 
 #in js
+var delNodes = function(root, to_delete) {
+    let res = {};
+    let to_delete_set = new Set(to_delete);
+    res[root.val] = root;
+
+    function recursion(parent, cur_node, isleft) {
+        if (cur_node === null) return;
+
+        recursion(cur_node, cur_node.left, true);
+        recursion(cur_node, cur_node.right, false);
+
+        if (to_delete_set.has(cur_node.val)) {
+            delete res[cur_node.val];
+
+            if (parent !== null) {
+                if (isleft) {
+                    parent.left = null;
+                } else {
+                    parent.right = null;
+                }
+            }
+
+            if (cur_node.left !== null) {
+                res[cur_node.left.val] = cur_node.left;
+            }
+            if (cur_node.right !== null) {
+                res[cur_node.right.val] = cur_node.right;
+            }
+        }
+    }
+
+    recursion(null, root, false);
+
+    return Object.values(res);
+};
