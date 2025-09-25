16bad70c4afebfc861ed3ee0b5af5fd3e5cc8c16
Yabets Maregn <yabetsanon@gmail.com>
2025-09-25 20:42:36 +0900

    Update 861. Score After Flipping Matrix.py

--- 861. Score After Flipping Matrix.py
+++ 861. Score After Flipping Matrix.py
@@ -7,3 +7,15 @@ class Solution:
             cur = sum(grid[i][j] == grid[i][0] for i in range(M))
             res += max(cur, M - cur) * (1 << (N - 1 - j))
         return res
+
+
+#in cpp
+    int matrixScore(vector<vector<int>> A) {
+        int M = A.size(), N = A[0].size(), res = (1 << (N - 1)) * M;
+        for (int j = 1; j < N; j++) {
+            int cur = 0;
+            for (int i = 0; i < M; i++) cur += A[i][j] == A[i][0];
+            res += max(cur, M - cur) * (1 << (N - j - 1));
+        }
+        return res;
+    }
