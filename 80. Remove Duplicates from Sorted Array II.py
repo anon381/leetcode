08d19141235d53eeae8e47102ebd97b47fcde741
Yabets Maregn <yabetsanon@gmail.com>
2025-09-26 01:39:26 +0900

    Add JavaScript implementation for removing duplicates

--- 80. Remove Duplicates from Sorted Array II.py
+++ 80. Remove Duplicates from Sorted Array II.py
@@ -46,3 +46,20 @@ class Solution {
         return k;        
     }
 }
+
+
+
+
+#in ja
+var removeDuplicates = function(nums) {
+    let k = 2;
+
+    for (let i = 2; i < nums.length; i++) {
+        if (nums[i] !== nums[k - 2]) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+
+    return k;    
+};
