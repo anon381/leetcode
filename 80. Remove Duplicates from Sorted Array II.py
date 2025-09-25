7157c09c5fe1a7bb553b0aaffbb38eb52885f0bc
Yabets Maregn <yabetsanon@gmail.com>
2025-09-26 01:38:31 +0900

    Update 80. Remove Duplicates from Sorted Array II.py

--- 80. Remove Duplicates from Sorted Array II.py
+++ 80. Remove Duplicates from Sorted Array II.py
@@ -9,3 +9,22 @@ class Solution:
                 k += 1 
 
         return k
+
+#in cpp
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int k = 2;
+
+        if (nums.size() <= 2) return nums.size();
+        
+        for (int i = 2; i < nums.size(); i++) {
+            if (nums[i] != nums[k - 2]) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;       
+    }
+};
