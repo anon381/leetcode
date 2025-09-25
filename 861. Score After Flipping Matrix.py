ad43cb706964b0cf712e8478cc1717cd38cfddab
Yabets Maregn <yabetsanon@gmail.com>
2025-09-25 20:42:14 +0900

    Update 861. Score After Flipping Matrix.py

--- 861. Score After Flipping Matrix.py
+++ 861. Score After Flipping Matrix.py
@@ -1,1 +1,9 @@
 
+class Solution:
+    def matrixScore(self, grid: List[List[int]]) -> int:
+        M, N = len(grid), len(grid[0])
+        res = (1 << (N - 1)) * M
+        for j in range(1, N):
+            cur = sum(grid[i][j] == grid[i][0] for i in range(M))
+            res += max(cur, M - cur) * (1 << (N - 1 - j))
+        return res
