9ce773147f258f6086ae4e8d98df70714f2df96b
Yabets Maregn <yabetsanon@gmail.com>
2025-09-25 20:30:34 +0900

    Update 870. Advantage Shuffle.py

--- 870. Advantage Shuffle.py
+++ 870. Advantage Shuffle.py
@@ -20,3 +20,17 @@ class Solution:
                 A.pop(0)
         
         return res
+
+#in cpp
+class Solution {
+public:
+       vector<int> advantageCount(vector<int>& A, vector<int>& B) {
+        multiset<int> s(begin(A), end(A));
+        for (int i = 0; i < B.size(); ++i) {
+            auto it = *s.rbegin() > B[i] ? s.upper_bound(B[i]) : s.begin();
+            A[i] = *it;
+            s.erase(it);
+        }
+        return A;
+    }
+};
