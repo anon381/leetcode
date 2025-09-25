ef82700b2a3271446593aa0858cee0aadbf0610c
Yabets Maregn <yabetsanon@gmail.com>
2025-09-25 20:07:35 +0900

    Implement numRescueBoats in Java
    Added Java implementation for numRescueBoats function.

--- 881. Boats to Save People.py
+++ 881. Boats to Save People.py
@@ -42,3 +42,29 @@ public:
         return boatCount;
     }
 };
+
+
+#in java
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        int boatCount = 0;
+        Arrays.sort(people);
+        
+        int left = 0;
+        int right = people.length - 1;
+        
+        while(left <= right){
+            int sum = people[left] + people[right];
+            if(sum <= limit){
+                boatCount++;
+                left++;
+                right--;
+            }
+            else{
+                boatCount++;
+                right--;
+            }
+        }
+        return boatCount;
+    }
+}
