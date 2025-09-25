ba77ffaf4060d325709484fc49e2aeee1e952f10
Yabets Maregn <yabetsanon@gmail.com>
2025-09-25 20:06:59 +0900

    Update 881. Boats to Save People.py

--- 881. Boats to Save People.py
+++ 881. Boats to Save People.py
@@ -1,1 +1,18 @@
 
+from typing import List
+
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        people.sort()
+        left, right = 0, len(people) - 1
+        boat_count = 0
+
+        while left <= right:
+            if people[left] + people[right] <= limit:
+                left += 1
+                right -= 1
+            else:
+                right -= 1
+            boat_count += 1
+
+        return boat_count
