44654b1919f2835a8208242c4bb3b5abd68114ff
Yabets Maregn <yabetsanon@gmail.com>
2025-09-28 02:18:18 +0900

    Update 1781. Sum of Beauty of All Substrings.py

--- 1781. Sum of Beauty of All Substrings.py
+++ 1781. Sum of Beauty of All Substrings.py
@@ -11,3 +11,30 @@ class Solution:
                 freq[s[j]] += 1
                 total_beauty += max(freq.values()) - min(freq.values())
         return total_beauty
+
+
+#in cpp
+class Solution {
+public:
+    int beautySum(string s) {
+        int beautysum = 0;
+        unordered_map<char,int> mp;
+    
+        for(int i=0; i<s.size(); i++){
+            mp.clear();
+            for(int j=i; j<s.size(); j++){
+                char &ch = s[j];
+                mp[ch]++;
+                int maxi = INT_MIN;
+                int mini = INT_MAX;
+                for(auto each:mp){
+                    maxi = max(maxi,each.second);
+                    mini = min(mini,each.second);
+                }
+                int beauty = maxi-mini;
+                beautysum += beauty;
+            }
+        }
+        return beautysum;
+    }
+};
