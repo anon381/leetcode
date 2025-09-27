89c2a743585d4b0a2fcf5da339480ea71093d058
Yabets Maregn <yabetsanon@gmail.com>
2025-09-27 18:48:41 +0900

    Add MySQL comment to game_analysis function

--- 511. Game Play Analysis I.py
+++ 511. Game Play Analysis I.py
@@ -3,3 +3,5 @@ import pandas as pd
 def game_analysis(activity: pd.DataFrame) -> pd.DataFrame:
     return (activity.groupby('player_id')['event_date'].min().reset_index()
                     .rename(columns = {'event_date':'first_login'}))
+
+#in mysql
