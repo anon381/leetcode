a9d90f0b51a6ee01ccfc22a93f88f0e6573cc946
Yabets Maregn <yabetsanon@gmail.com>
2025-09-28 22:35:25 +0900

    Update 1700. Number of Students Unable to Eat Lunch.py

--- 1700. Number of Students Unable to Eat Lunch.py
+++ 1700. Number of Students Unable to Eat Lunch.py
@@ -40,3 +40,20 @@ public:
         return remaining;
     }
 };
+
+#in java
+class Solution {
+    public int countStudents(int[] students, int[] sandwiches) {
+        int[] counts = new int[2];
+        for (int student : students) counts[student]++;
+        
+        int remaining = sandwiches.length;
+        for (int sandwich : sandwiches) {
+            if (counts[sandwich] == 0) break;
+            if (remaining-- == 0) break;
+            counts[sandwich]--;
+        }
+        
+        return remaining;
+    }
+}
