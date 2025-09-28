54199e636daea2ef60f672f4efc58e811aa3dd4e
Yabets Maregn <yabetsanon@gmail.com>
2025-09-28 20:45:48 +0900

    Update 3649. Number of Perfect Pairs.py

--- 3649. Number of Perfect Pairs.py
+++ 3649. Number of Perfect Pairs.py
@@ -1,1 +1,16 @@
 
+class Solution:
+    def perfectPairs(self, nums: List[int]) -> int:
+
+        nums = [abs(x) for x in nums]
+        nums.sort()
+        
+        n, left = len(nums), 0
+        ans = (n - 1) * n // 2
+        
+        for rght, num in enumerate(nums):
+            while left < rght and num > 2 * nums[left]:
+                left+= 1
+            ans-= left
+            
+        return ans
