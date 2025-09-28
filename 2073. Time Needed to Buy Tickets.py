577ecad916bfbecfb0d9ce6606d7134a69ae658a
Yabets Maregn <yabetsanon@gmail.com>
2025-09-28 22:50:28 +0900

    Implement timeRequiredToBuy method in Solution class

--- 2073. Time Needed to Buy Tickets.py
+++ 2073. Time Needed to Buy Tickets.py
@@ -1,1 +1,10 @@
-
+class Solution:
+    def timeRequiredToBuy(self, t: List[int], k: int) -> int:
+        n=len(t)
+        x=t[k]
+        time=0
+        for i, y in enumerate(t):
+            buy=x
+            if i>k: buy=x-1
+            time+=min(buy, y)
+        return time
