79492e9d1c1e172d0d050fe48b22045c950ce7db
Yabets Maregn <yabetsanon@gmail.com>
2025-10-05 21:17:22 +0900

    Update 1884. Egg Drop With 2 Eggs and N Floors.py

--- 1884. Egg Drop With 2 Eggs and N Floors.py
+++ 1884. Egg Drop With 2 Eggs and N Floors.py
@@ -4,3 +4,15 @@ import math
 def twoEggDrop(n: int) -> int:
     return math.ceil((-1 + math.sqrt(1 + 8*n)) / 2)
 
+
+#in cpp
+#include <cmath>
+using namespace std;
+
+class Solution {
+public:
+    int twoEggDrop(int n) {
+        // smallest k such that k*(k+1)/2 >= n
+        return ceil((-1 + sqrt(1 + 8.0 * n)) / 2);
+    }
+};
