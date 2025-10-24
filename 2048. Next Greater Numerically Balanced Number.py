1be59f023a702bb489654fa6856cd6234db22408
Yabets Maregn <yabetsanon@gmail.com>
2025-10-24 16:15:22 +0900

    Implement nextBeautifulNumber function

--- 2048. Next Greater Numerically Balanced Number.py
+++ 2048. Next Greater Numerically Balanced Number.py
@@ -1,1 +1,15 @@
 
+class Solution:
+    def nextBeautifulNumber(self, n: int) -> int:
+        def balanced(x):
+            freq = Counter(str(x))
+            for key, val in freq.items():
+                if int(key) != val:
+                    return False
+            return True
+
+        num = n + 1
+        while True:
+            if balanced(num):
+                return num
+            num += 1
