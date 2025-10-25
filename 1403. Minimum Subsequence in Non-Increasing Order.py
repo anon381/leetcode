8954282ab9f885a1b56f37695d44027a3e2ded68
Yabets Maregn <yabetsanon@gmail.com>
2025-10-25 21:49:08 +0900

    Update 1403. Minimum Subsequence in Non-Increasing Order.py

--- 1403. Minimum Subsequence in Non-Increasing Order.py
+++ 1403. Minimum Subsequence in Non-Increasing Order.py
@@ -1,1 +1,16 @@
+class Solution:
+    def minSubsequence(self, nums: List[int]) -> List[int]:
+        nums.sort()
+        y = sum(nums)
+        nums.reverse()
+        a,count = 0,0
+        for i in nums:
+            if a+i > y -(a+i):
+                break
+            else:
+                a+=i
+            count+=1
+        return(nums[0:count+1])
+
+
 
