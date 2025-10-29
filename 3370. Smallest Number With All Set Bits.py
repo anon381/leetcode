8436858feb875f485090bf752a8d9c54f83bd3d0
Yabets Maregn <yabetsanon@gmail.com>
2025-10-29 15:41:59 +0900

    Update 3370. Smallest Number With All Set Bits.py

--- 3370. Smallest Number With All Set Bits.py
+++ 3370. Smallest Number With All Set Bits.py
@@ -2,3 +2,13 @@
 class Solution:
     def smallestNumber(self, n: int) -> int:
         return (1<<(1+int(log2(n))))-1
+#in cpp
+class Solution {
+public:
+    int smallestNumber(unsigned n) {
+        int x=1023, prev;
+        for(; x>=n; x>>=1)
+            prev=x;
+        return prev;
+    }
+};
