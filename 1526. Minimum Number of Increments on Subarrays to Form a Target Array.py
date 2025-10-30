84d0b881defef141b376f4227253ded9f6ac410e
Yabets Maregn <yabetsanon@gmail.com>
2025-10-30 17:34:26 +0900

    Update 1526. Minimum Number of Increments on Subarrays to Form a Target Array.py

--- 1526. Minimum Number of Increments on Subarrays to Form a Target Array.py
+++ 1526. Minimum Number of Increments on Subarrays to Form a Target Array.py
@@ -39,3 +39,56 @@ class Solution:
                 q.append((idx + 1, r, mini))
 
         return ans
+
+
+#in cpp
+class Solution {
+public:
+    vector<pair<int,int>> seg;
+
+    // Build the segment tree
+    void build(int ind, int low, int high, vector<int>& target){
+        if(low == high){
+            seg[ind] = {target[low], low}; // {MIN VALUE, INDEX}
+            return;
+        }
+        int mid = (low + high)/2;
+        build(2*ind+1, low, mid, target);
+        build(2*ind+2, mid+1, high, target);
+        seg[ind] = min( seg[2*ind+1], seg[2*ind+2] );
+        return;
+    }
+
+    // Find the {minimum value, index} from range l to r
+    pair<int,int> query(int ind, int low, int high, int l, int r, vector<int>& target){
+        if(high < l || low > r) return {INT_MAX, INT_MAX}; // NO OVERLAP
+        if( low >= l && high <= r ) return seg[ind]; // COMPLETE OVERLAP
+
+        int mid = (low + high)/2; // PARTIAL OVERLAP
+        auto left = query(2*ind+1, low, mid, l, r, target); // MIN OF LEFT HALF
+        auto right = query(2*ind+2, mid+1, high, l,r, target); // MIN OF RIGHT HALF
+
+        return min(left, right); // MIN OF BOTH
+    }
+
+    int minNumberOperations(vector<int>& target) {
+        int n = target.size();
+        seg.resize(4*n);
+        build(0,0,n-1, target); // build the segment tree
+
+        int ans = 0;
+        queue<vector<int>> q;
+        q.push({0,n-1,0}); // LEFT LIMIT | RIGHT LIMIT | BASE LEVEL
+        while(!q.empty()){
+            auto it = q.front();
+            q.pop();
+            int l = it[0], r = it[1], level = it[2];
+            auto que = query(0,0,n-1,l,r,target);
+            int mini = que.first, ind = que.second;
+            ans += (mini - level); // add the extra increment needed from base level
+            if(ind-1 >= l) q.push({l, ind-1, mini}); // push the left half
+            if(ind+1 <= r) q.push({ind+1, r, mini}); // push the right half
+        }
+        return ans;
+    }
+};
