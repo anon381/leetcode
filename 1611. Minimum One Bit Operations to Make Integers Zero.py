ed7b2344d08c720d1ba3d8de9f3e3e8fe6081589
Yabets Maregn <yabetsanon@gmail.com>
2025-11-08 14:28:30 +0900

    Add C++ solution for minimumOneBitOperations
    Added C++ implementation of minimumOneBitOperations function.

--- 1611. Minimum One Bit Operations to Make Integers Zero.py
+++ 1611. Minimum One Bit Operations to Make Integers Zero.py
@@ -5,3 +5,15 @@ class Solution:
             res ^= n
             n >>= 1
         return res
+#in cpp
+class Solution {
+public:
+    int minimumOneBitOperations(int n) {
+        int res = 0;
+        while (n) {
+            res ^= n;
+            n >>= 1;
+        }
+        return res;
+    }
+};
