7a32899f53b31c0d9f0a1154923ea90d3371da81
Yabets Maregn <yabetsanon@gmail.com>
2025-11-08 14:28:46 +0900

    Implement minimumOneBitOperations in Java
    Added Java implementation of the minimum one bit operations algorithm.

--- 1611. Minimum One Bit Operations to Make Integers Zero.py
+++ 1611. Minimum One Bit Operations to Make Integers Zero.py
@@ -17,3 +17,14 @@ public:
         return res;
     }
 };
+#in java
+class Solution {
+    public int minimumOneBitOperations(int n) {
+        int res = 0;
+        while (n > 0) {
+            res ^= n;
+            n >>= 1;
+        }
+        return res;
+    }
+}
