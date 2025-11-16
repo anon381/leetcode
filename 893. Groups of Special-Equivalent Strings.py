aa73a9ed6a90d3aee8772eb9a7181dfcbf7cb73e
Yabets Maregn <yabetsanon@gmail.com>
2025-11-16 18:16:33 +0900

    Add numSpecialEquivGroups method to Solution class
    Implement a solution to count special-equivalent groups of strings.

--- 893. Groups of Special-Equivalent Strings.py
+++ 893. Groups of Special-Equivalent Strings.py
@@ -1,1 +1,21 @@
 
+class Solution:
+    def numSpecialEquivGroups(self, A):
+        s = set()
+
+        for w in A:
+            even = []
+            odd = []
+
+            for i, ch in enumerate(w):
+                if i % 2 == 0:
+                    even.append(ch)
+                else:
+                    odd.append(ch)
+
+            even.sort()
+            odd.sort()
+
+            s.add(("".join(even), "".join(odd)))
+
+        return len(s)
