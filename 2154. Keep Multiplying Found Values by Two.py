d4c514c5e632618e9e1937b649199e10154cdb28
Yabets Maregn <yabetsanon@gmail.com>
2025-11-19 15:13:33 +0900

    Update 2154. Keep Multiplying Found Values by Two.py

--- 2154. Keep Multiplying Found Values by Two.py
+++ 2154. Keep Multiplying Found Values by Two.py
@@ -9,3 +9,19 @@ class Solution:
                 bits |= n
         d = bits + 1
         return k * (d & -d)
+
+#in cpp
+class Solution {
+public:
+    int findFinalValue(vector<int>& nums, int k) {
+        int bits = 0;
+        for (auto& n : nums) {
+            if (n % k != 0) continue;
+            n = n / k;
+            if ((n & (n - 1)) == 0)
+                bits |= n;
+        }
+        int d = bits + 1;
+        return k * (d & -d);
+    }
+};
