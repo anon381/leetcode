47cbc87964c7bac732af351767db5f615b0f3371
Yabets Maregn <yabetsanon@gmail.com>
2025-12-20 22:10:52 +0900

    Update 434. Number of Segments in a String.py

--- 434. Number of Segments in a String.py
+++ 434. Number of Segments in a String.py
@@ -1,1 +1,8 @@
 
+class Solution:
+    def countSegments(self, s: str) -> int:
+        c =0
+        for i in range(len(s)):
+            if s[i] != ' ' and (i == 0 or s[i-1] == ' '):
+                c+=1
+        return c
