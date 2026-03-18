e79917856248480c2eee2dc8c253b19d0e970b82
Yabets Maregn <yabetsanon@gmail.com>
2026-03-18 17:55:55 +0300

    Create         1893. Check if All the Integers in a Range Are Covered.py

---
+++ 1893. Check if All the Integers in a Range Are Covered.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isCovered(self, ranges: List[List[int]], left: int, right: int) -> bool:
+        for x in range(left, right + 1):
+            for start, end in ranges:
+                if start <= x <= end:
+                    break
+            else:
+                return False
+        
+        return True
+        
+        1893. Check if All the Integers in a Range Are Covered
